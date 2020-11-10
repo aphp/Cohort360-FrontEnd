33cd8507b3e2777d0cefa58f90c68fc05720559c
Salah BOUYAHIA <salah.bouyahia-ext@aphp.fr>
2020-11-10 15:36:09 +0100

    [2.1] fix/format fix on criteria json

--- src/data/Requeteur/VISITE/admissionMode.js
+++ src/data/Requeteur/VISITE/admissionMode.js
@@ -1,39 +1,38 @@
 export default [
-    {
-        'admissionModeCode': '01',
-        'label': 'Admission générale'
-    },
-    {
-        'admissionModeCode': '02',
-        'label': 'Admission réduite'
-    },
-    {
-        'admissionModeCode': '03',
-        'label': 'Emergency'
-    },
-    {
-        'admissionModeCode':'04',
-        'label': 'Institute'
-    },
-    {
-        'admissionModeCode':'05',
-        'label': 'Interface'
-    },
-    {
-        'admissionModeCode':'06',
-        'label': 'Legacy data'
-    },
-    {
-        'admissionModeCode':'07',
-        'label': 'Non renseigné'
-    },
-    {
-        'admissionModeCode':'08',
-        'label': 'Pre'
-    },
-    {
-        'admissionModeCode':'09',
-        'label': 'Web'
-
-    }
+  {
+    admissionModeCode: '01',
+    label: 'Admission générale'
+  },
+  {
+    admissionModeCode: '02',
+    label: 'Admission réduite'
+  },
+  {
+    admissionModeCode: '03',
+    label: 'Emergency'
+  },
+  {
+    admissionModeCode: '04',
+    label: 'Institute'
+  },
+  {
+    admissionModeCode: '05',
+    label: 'Interface'
+  },
+  {
+    admissionModeCode: '06',
+    label: 'Legacy data'
+  },
+  {
+    admissionModeCode: '07',
+    label: 'Non renseigné'
+  },
+  {
+    admissionModeCode: '08',
+    label: 'Pre'
+  },
+  {
+    admissionModeCode: '09',
+    label: 'Web'
+  }
 ]
