0b90a065b5b757e3780fb13e60871ebaa8366040
vmariot <mariotvictor@gmail.com>
2020-12-02 20:15:05 +0100

    [2.1][Fix] - Remove console.log

--- src/components/Cohort/CreationCohort/Requeteur.tsx
+++ src/components/Cohort/CreationCohort/Requeteur.tsx
@@ -71,9 +71,7 @@ const Requeteur = () => {
   }, []) // eslint-disable-line
 
   useEffect(() => {
-    console.log('selectedCriteria', selectedCriteria)
     const requeteurJson = buildRequest(selectedCriteria)
-    console.log('requeteurJson', requeteurJson)
   }, [selectedCriteria])
 
   if (loading) return <CircularProgress />
