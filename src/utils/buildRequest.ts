fb1a1b2cacad53c61bf264e6be819954f4d43d05
vmariot <mariotvictor@gmail.com>
2020-12-11 19:10:47 +0100

    [2.1][Feat] - Work on unbuildRequest

--- src/utils/buildRequest.ts
+++ src/utils/buildRequest.ts
@@ -3,9 +3,43 @@ import moment from 'moment'
 import { fetchPerimeterInfoForRequeteur as fetchPopulation } from '../services/perimeters'
 import { ScopeTreeRow, SelectedCriteriaType } from 'types'
 
+const RESSOURCE_TYPE_PATIENT: 'Patient' = 'Patient'
+const PATIENT_GENDER = 'gender'
+const PATIENT_BIRTHDATE = 'birthdate'
+const PATIENT_DECEASED = 'deceased'
+
+const RESSOURCE_TYPE_ENCOUNTER: 'Encounter' = 'Encounter'
+const ENCOUNTER_LENGTH = 'length'
+const ENCOUNTER_ADMISSIONMODE = 'admissionMode'
+const ENCOUNTER_ENTRYMODE = 'entryMode'
+const ENCOUNTER_EXITMODE = 'exitMode'
+const ENCOUNTER_FILESTATUS = 'fileStatus'
+
+const RESSOURCE_TYPE_CLAIM: 'Claim' = 'Claim'
+const CLAIM_CODE = 'code'
+const CLAIM_CREATED = 'created'
+
+const RESSOURCE_TYPE_PROCEDURE: 'Procedure' = 'Procedure'
+const PROCEDURE_CODE = 'code'
+const PROCEDURE_DATE = 'date'
+
+const RESSOURCE_TYPE_CONDITION: 'Condition' = 'Condition'
+const CONDITION_CODE = 'code'
+const CONDITION_TYPE = 'type'
+
+const RESSOURCE_TYPE_COMPOSITION: 'Composition' = 'Composition'
+const COMPOSITION_TEXT = 'text'
+const COMPOSITION_TYPE = 'type'
+
 type RequeteurSearchType = {
   _type: string
-  resourceType: 'Patient' | 'Encounter' | 'Claim' | 'Procedure' | 'Condition' | 'Composition'
+  resourceType:
+    | typeof RESSOURCE_TYPE_PATIENT
+    | typeof RESSOURCE_TYPE_ENCOUNTER
+    | typeof RESSOURCE_TYPE_CLAIM
+    | typeof RESSOURCE_TYPE_PROCEDURE
+    | typeof RESSOURCE_TYPE_CONDITION
+    | typeof RESSOURCE_TYPE_COMPOSITION
   fhirFilter: string
 }
 
@@ -21,7 +55,7 @@ export function buildRequest(selectedPopulation: any, selectedCriteria: any) {
   let newJson: RequeteurSearchType[] = [
     {
       _type: 'resource',
-      resourceType: 'Patient',
+      resourceType: RESSOURCE_TYPE_PATIENT,
       fhirFilter: `_list=${selectedPopulation
         .map((selectedPopulation: any) => selectedPopulation.id)
         .reduce(searchReducer)}`
@@ -32,55 +66,56 @@ export function buildRequest(selectedPopulation: any, selectedCriteria: any) {
     let fhirFilter = ''
 
     switch (selectedCriterion.type) {
-      case 'Patient': {
+      case RESSOURCE_TYPE_PATIENT: {
         let ageFilter = ''
         if (selectedCriterion.years && selectedCriterion.years !== [0, 100]) {
           const date1 = moment().subtract(selectedCriterion.years[1], 'years').format('YYYY-MM-DD')
           const date2 = moment().subtract(selectedCriterion.years[0], 'years').format('YYYY-MM-DD')
-          ageFilter = `birthdate=ge${date1},le${date2}`
+          ageFilter = `${PATIENT_BIRTHDATE}=ge${date1},le${date2}`
         }
-
         fhirFilter = [
-          `${selectedCriterion.gender ? `gender=${selectedCriterion.gender.id}` : ''}`,
-          `${selectedCriterion.vitalStatus ? `deceased=${selectedCriterion.vitalStatus.id}` : ''}`,
+          `${selectedCriterion.gender ? `${PATIENT_GENDER}=${selectedCriterion.gender.id}` : ''}`,
+          `${selectedCriterion.vitalStatus ? `${PATIENT_DECEASED}=${selectedCriterion.vitalStatus.id}` : ''}`,
           `${ageFilter ? `${ageFilter}` : ''}`
         ].reduce(filterReducer)
         break
       }
 
-      case 'Encounter': {
+      case RESSOURCE_TYPE_ENCOUNTER: {
         let lengthFilter = ''
         if (selectedCriterion.duration && selectedCriterion.duration !== [0, 100]) {
-          lengthFilter = `length=ge${selectedCriterion.duration[0]},le${selectedCriterion.duration[1]}`
+          lengthFilter = `${ENCOUNTER_LENGTH}=ge${selectedCriterion.duration[0]},le${selectedCriterion.duration[1]}`
         }
 
         fhirFilter = [
-          `${selectedCriterion.admissionMode ? `admissionMode=${selectedCriterion.admissionMode.id}` : ''}`,
-          `${selectedCriterion.entryMode ? `entryMode=${selectedCriterion.entryMode.id}` : ''}`,
-          `${selectedCriterion.exitMode ? `exitMode=${selectedCriterion.exitMode.id}` : ''}`,
-          `${selectedCriterion.fileStatus ? `fileStatus=${selectedCriterion.fileStatus.id}` : ''}`,
+          `${
+            selectedCriterion.admissionMode ? `${ENCOUNTER_ADMISSIONMODE}=${selectedCriterion.admissionMode.id}` : ''
+          }`,
+          `${selectedCriterion.entryMode ? `${ENCOUNTER_ENTRYMODE}=${selectedCriterion.entryMode.id}` : ''}`,
+          `${selectedCriterion.exitMode ? `${ENCOUNTER_EXITMODE}=${selectedCriterion.exitMode.id}` : ''}`,
+          `${selectedCriterion.fileStatus ? `${ENCOUNTER_FILESTATUS}=${selectedCriterion.fileStatus.id}` : ''}`,
           `${lengthFilter ? `${lengthFilter}` : ''}`
         ].reduce(filterReducer)
         break
       }
 
-      case 'Composition': {
+      case RESSOURCE_TYPE_COMPOSITION: {
         fhirFilter = [
-          `${selectedCriterion.search ? `text=${selectedCriterion.search}` : ''}`,
-          `${selectedCriterion.docType ? `type=${selectedCriterion.docType.id}` : ''}`
+          `${selectedCriterion.search ? `${COMPOSITION_TEXT}=${selectedCriterion.search}` : ''}`,
+          `${selectedCriterion.docType ? `${COMPOSITION_TYPE}=${selectedCriterion.docType.id}` : ''}`
         ].reduce(filterReducer)
         break
       }
 
-      case 'Condition': {
+      case RESSOURCE_TYPE_CONDITION: {
         fhirFilter = [
-          `${selectedCriterion.code ? `code=${selectedCriterion.code.id}` : ''}`,
-          `${selectedCriterion.diagnosticType ? `type=${selectedCriterion.diagnosticType.id}` : ''}`
+          `${selectedCriterion.code ? `${CONDITION_CODE}=${selectedCriterion.code.id}` : ''}`,
+          `${selectedCriterion.diagnosticType ? `${CONDITION_TYPE}=${selectedCriterion.diagnosticType.id}` : ''}`
         ].reduce(filterReducer)
         break
       }
 
-      case 'Procedure': {
+      case RESSOURCE_TYPE_PROCEDURE: {
         let dateFilter = ''
         if (selectedCriterion.startOccurrence || selectedCriterion.endOccurrence) {
           dateFilter = [
@@ -92,13 +127,13 @@ export function buildRequest(selectedPopulation: any, selectedCriteria: any) {
         }
 
         fhirFilter = [
-          `${selectedCriterion.code ? `code=${selectedCriterion.code.id}` : ''}`,
-          `${dateFilter ? `date=${dateFilter}` : ''}`
+          `${selectedCriterion.code ? `${PROCEDURE_CODE}=${selectedCriterion.code.id}` : ''}`,
+          `${dateFilter ? `${PROCEDURE_DATE}=${dateFilter}` : ''}`
         ].reduce(filterReducer)
         break
       }
 
-      case 'Claim': {
+      case RESSOURCE_TYPE_CLAIM: {
         let dateFilter = ''
         if (selectedCriterion.startOccurrence || selectedCriterion.endOccurrence) {
           dateFilter = [
@@ -114,8 +149,8 @@ export function buildRequest(selectedPopulation: any, selectedCriteria: any) {
         }
 
         fhirFilter = [
-          `${selectedCriterion.code ? `code=${selectedCriterion.code.id}` : ''}`,
-          `${dateFilter ? `created=${dateFilter}` : ''}`
+          `${selectedCriterion.code ? `${CLAIM_CODE}=${selectedCriterion.code.id}` : ''}`,
+          `${dateFilter ? `${CLAIM_CREATED}=${dateFilter}` : ''}`
         ].reduce(filterReducer)
         break
       }
@@ -153,39 +188,152 @@ export async function unbuildRequest(json: string) {
   let criteria: SelectedCriteriaType[] = []
 
   const _retrieveInformationFromJson = async (element: RequeteurSearchType) => {
-    const currentCriterion = {
+    const currentCriterion: any = {
       type: element.resourceType,
-      title: '',
-      fhirFilter: ''
+      title: ''
     }
     switch (element.resourceType) {
-      case 'Patient': {
+      case RESSOURCE_TYPE_PATIENT: {
         if (element.fhirFilter) {
           const filters = element.fhirFilter.split('&').map((elem) => elem.split('='))
+          console.log('filters', filters)
           for (const filter of filters) {
             const key = filter ? filter[0] : null
             const value = filter ? filter[1] : null
-
             if (key === '_list') {
               population = await fetchPopulation(value ?? '')
               return
             } else {
-              //
+              currentCriterion.title = 'Critère démographique'
+              currentCriterion.years = currentCriterion.years ? currentCriterion.years : null
+              currentCriterion.gender = currentCriterion.gender ? currentCriterion.gender : null
+              currentCriterion.vitalStatus = currentCriterion.vitalStatus ? currentCriterion.vitalStatus : null
+              switch (key) {
+                case PATIENT_BIRTHDATE: {
+                  const dateValues = value?.split(',')
+                  const date2 = dateValues ? moment(dateValues[0].replace('le', ''), 'YYYY-MM-DD') : null
+                  const date1 = dateValues ? moment(dateValues[1].replace('ge', ''), 'YYYY-MM-DD') : null
+                  currentCriterion.years = [
+                    date1 ? moment().diff(date1, 'years') : 0,
+                    date2 ? moment().diff(date2, 'years') : 100
+                  ]
+                  break
+                }
+                case PATIENT_GENDER:
+                  currentCriterion.gender = { id: value }
+                  break
+                case PATIENT_DECEASED:
+                  currentCriterion.vitalStatus = { id: value }
+                  break
+                default:
+                  break
+              }
             }
           }
         }
         break
       }
-      case 'Encounter': {
+      case RESSOURCE_TYPE_ENCOUNTER: {
+        if (element.fhirFilter) {
+          const filters = element.fhirFilter.split('&').map((elem) => elem.split('='))
+          for (const filter of filters) {
+            const key = filter ? filter[0] : null
+            const value = filter ? filter[1] : null
+            switch (key) {
+              case ENCOUNTER_LENGTH: {
+                const lengthValues = value?.split(',')
+                currentCriterion.duration = [
+                  lengthValues ? lengthValues[0].replace('le', '') : 0,
+                  lengthValues ? lengthValues[1].replace('ge', '') : 0
+                ]
+                break
+              }
+              case ENCOUNTER_ADMISSIONMODE:
+                currentCriterion.admissionMode = { id: value }
+                break
+              case ENCOUNTER_ENTRYMODE:
+                currentCriterion.entryMode = { id: value }
+                break
+              case ENCOUNTER_EXITMODE:
+                currentCriterion.exitMode = { id: value }
+                break
+              case ENCOUNTER_FILESTATUS:
+                currentCriterion.fileStatus = { id: value }
+                break
+
+              default:
+                break
+            }
+          }
+        }
         break
       }
-      case 'Condition': {
+      case RESSOURCE_TYPE_COMPOSITION: {
+        if (element.fhirFilter) {
+          const filters = element.fhirFilter.split('&').map((elem) => elem.split('='))
+          for (const filter of filters) {
+            const key = filter ? filter[0] : null
+            const value = filter ? filter[1] : null
+            switch (key) {
+              case COMPOSITION_TEXT:
+                currentCriterion.search = value
+                break
+              case COMPOSITION_TYPE:
+                currentCriterion.docType = { id: value }
+                break
+              default:
+                break
+            }
+          }
+        }
         break
       }
-      case 'Procedure': {
+      case RESSOURCE_TYPE_CONDITION: {
+        if (element.fhirFilter) {
+          const filters = element.fhirFilter.split('&').map((elem) => elem.split('='))
+          for (const filter of filters) {
+            const key = filter ? filter[0] : null
+            const value = filter ? filter[1] : null
+            switch (key) {
+              case COMPOSITION_TEXT:
+                currentCriterion.code = { id: value }
+                break
+              case COMPOSITION_TYPE:
+                currentCriterion.diagnosticType = { id: value }
+                break
+              default:
+                break
+            }
+          }
+        }
         break
       }
-      case 'Claim': {
+      case RESSOURCE_TYPE_PROCEDURE: {
+        if (element.fhirFilter) {
+          const filters = element.fhirFilter.split('&').map((elem) => elem.split('='))
+          for (const filter of filters) {
+            const key = filter ? filter[0] : null
+            const value = filter ? filter[1] : null
+            switch (key) {
+              default:
+                break
+            }
+          }
+        }
+        break
+      }
+      case RESSOURCE_TYPE_CLAIM: {
+        if (element.fhirFilter) {
+          const filters = element.fhirFilter.split('&').map((elem) => elem.split('='))
+          for (const filter of filters) {
+            const key = filter ? filter[0] : null
+            const value = filter ? filter[1] : null
+            switch (key) {
+              default:
+                break
+            }
+          }
+        }
         break
       }
       default:
