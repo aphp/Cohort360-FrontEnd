d11869b4b18bf416aa10e44583671d567195b8a8
vmariot <mariotvictor@gmail.com>
2021-01-07 17:01:17 +0100

    [2.1][Fix] - Work on cohort creation  - Link created cohort by cohort360 with i2b2 cohort (listing - link after creation ...)  - Add protection and "finish" cohort creation (name + desc.)

---
+++ src/components/Cohort/CreationCohort/ControlPanel/components/ModalCohortTitle/styles.ts
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() => ({
+  title: {
+    ' & h2': {
+      fontSize: '18px',
+      fontFamily: "'Montserrat', sans-serif",
+      color: '#0063AF',
+      textTransform: 'none',
+      lineHeight: 2
+    }
+  },
+  inputContainer: {
+    marginBottom: '24px'
+  }
+}))
+
+export default useStyles
