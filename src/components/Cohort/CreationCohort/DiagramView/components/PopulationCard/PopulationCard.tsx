7144c31ce19b4fe287656f964e2ed6fde260c295
manelleg <manelle.gueriouz@aphp.fr>
2021-03-02 09:53:50 +0100

    [2.2][feat] - deleted background for the add population button

--- src/components/Cohort/CreationCohort/DiagramView/components/PopulationCard/PopulationCard.tsx
+++ src/components/Cohort/CreationCohort/DiagramView/components/PopulationCard/PopulationCard.tsx
@@ -29,44 +29,44 @@ const PopulationCard: React.FC = () => {
 
   return (
     <>
-      <div className={classes.newPopulationCard}>
-        {loading ? (
+      {loading ? (
+        <div className={classes.newPopulationCard}>
           <div className={classes.centerContainer}>
             <CircularProgress />
           </div>
-        ) : selectedPopulation !== null ? (
-          <>
-            <div className={classes.leftDiv}>
-              <Typography variant="h6" align="left">
-                Population source :
-              </Typography>
+        </div>
+      ) : selectedPopulation !== null ? (
+        <div className={classes.newPopulationCard}>
+          <div className={classes.leftDiv}>
+            <Typography variant="h6" align="left">
+              Population source :
+            </Typography>
 
-              <div className={classes.chipContainer}>
-                {selectedPopulation &&
-                  selectedPopulation
-                    .slice(0, 4)
-                    .map((pop: any, index: number) => (
-                      <Chip className={classes.populationChip} key={`${index}-${pop.name}`} label={pop.name} />
-                    ))}
-                {selectedPopulation && selectedPopulation.length > 4 && (
-                  <Typography component="span" align="center" className={classes.populationLabel}>
-                    ...
-                  </Typography>
-                )}
-              </div>
+            <div className={classes.chipContainer}>
+              {selectedPopulation &&
+                selectedPopulation
+                  .slice(0, 4)
+                  .map((pop: any, index: number) => (
+                    <Chip className={classes.populationChip} key={`${index}-${pop.name}`} label={pop.name} />
+                  ))}
+              {selectedPopulation && selectedPopulation.length > 4 && (
+                <Typography component="span" align="center" className={classes.populationLabel}>
+                  ...
+                </Typography>
+              )}
             </div>
-            <IconButton size="small" onClick={() => onChangeOpenDrawer(true)} style={{ color: 'currentcolor' }}>
-              <EditIcon />
-            </IconButton>
-          </>
-        ) : (
-          <div className={classes.centerContainer}>
-            <Button className={classes.actionButton} onClick={() => onChangeOpenDrawer(true)}>
-              Choisir une population source
-            </Button>
           </div>
-        )}
-      </div>
+          <IconButton size="small" onClick={() => onChangeOpenDrawer(true)} style={{ color: 'currentcolor' }}>
+            <EditIcon />
+          </IconButton>
+        </div>
+      ) : (
+        <div className={classes.centerContainer}>
+          <Button className={classes.actionButton} onClick={() => onChangeOpenDrawer(true)}>
+            Choisir une population source
+          </Button>
+        </div>
+      )}
 
       <PopulationRightPanel open={openDrawer} onConfirm={submitPopulation} onClose={() => onChangeOpenDrawer(false)} />
     </>
