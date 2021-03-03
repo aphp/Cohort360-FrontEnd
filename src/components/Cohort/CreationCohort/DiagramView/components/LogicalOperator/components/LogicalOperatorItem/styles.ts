099f1ef37861e65d096558374a05978fe06b6d9e
vmariot <mariotvictor@gmail.com>
2021-03-03 16:46:25 +0100

    [2.2][Feat] - Can edit a logical operator

--- src/components/Cohort/CreationCohort/DiagramView/components/LogicalOperator/components/LogicalOperatorItem/styles.ts
+++ src/components/Cohort/CreationCohort/DiagramView/components/LogicalOperator/components/LogicalOperatorItem/styles.ts
@@ -12,7 +12,6 @@ const useStyles = makeStyles(() => ({
     minHeight: 42,
     borderRadius: 5,
     textAlign: 'center',
-    lineHeight: '42px',
     marginTop: 12,
     position: 'relative',
     '&::after': {
@@ -21,32 +20,33 @@ const useStyles = makeStyles(() => ({
       height: 12,
       width: 3,
       background: '#19235a',
-      marginTop: 42,
-      marginLeft: -11
+      top: '100%',
+      left: 19
     }
   },
   logicalOperator: {
     background: '#19235A',
     color: 'white',
-    width: 42,
     height: 42,
-    maxWidth: 42,
-    maxHeight: 42,
     minWidth: 42,
     minHeight: 42,
+    width: 'fit-content !important',
+    maxWidth: 'inherit !important',
     borderRadius: 5,
     textAlign: 'center',
-    lineHeight: '42px',
     marginTop: 12,
     position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'left',
     '&::before': {
       position: 'absolute',
       content: "''",
       height: 3,
       width: 35,
       background: '#19235a',
-      marginTop: 19.5,
-      marginLeft: -47
+      top: 19.5,
+      left: -35
     },
     '&::after': {
       position: 'absolute',
@@ -54,8 +54,8 @@ const useStyles = makeStyles(() => ({
       height: 12,
       width: 3,
       background: '#19235a',
-      marginTop: 42,
-      marginLeft: -11
+      top: '100%',
+      left: 19
     }
   }
 }))
