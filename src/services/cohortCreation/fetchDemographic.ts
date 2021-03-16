a039069109b79006b4024ab6ab7fdd7873881d20
manelleg <manelle.gueriouz@aphp.fr>
2021-03-16 15:50:25 +0100

    [2.2][bug-fix] - translation of gender value set

--- src/services/cohortCreation/fetchDemographic.ts
+++ src/services/cohortCreation/fetchDemographic.ts
@@ -30,7 +30,31 @@ export const fetchGender = async () => {
       const res = await apiRequest.get(`/ValueSet?url=http://hl7.org/fhir/CodeSystem/administrative-gender`)
       const data = res.data.entry[0].resource.compose.include[0].concept || []
 
-      return cleanValueSet(data)
+      if (data && data.length > 0) {
+        data.forEach((value: { code: string; display: string }) => {
+          switch (value.code) {
+            case 'female':
+              value.display = 'Femmes'
+              break
+            case 'male':
+              value.display = 'Hommes'
+              break
+            case 'other':
+              value.display = 'Autres'
+              break
+            case 'unknown':
+              value.display = 'Non renseigné'
+              break
+            default:
+              value.display = 'Indéterminé'
+              break
+          }
+        })
+
+        return cleanValueSet(data)
+      } else {
+        return []
+      }
     } catch (error) {
       return []
     }
