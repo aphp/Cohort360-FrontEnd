43082688534ccccee8aa50be61e2da829f4436d5
Teva RIOU <teva.riou@gmail.com>
2021-03-22 21:31:17 +0100

    fix(arkhn): remove practitioner header at logout

--- src/services/api.js
+++ src/services/api.js
@@ -1,50 +1,43 @@
 import axios from 'axios'
 import { FHIR_API_URL, PRACTITIONER_ID } from '../constants'
-// import { refreshToken, removeTokens } from './arkhnAuth/oauth/tokenManager'
+import { removeTokens } from './arkhnAuth/oauth/tokenManager'
 
 const api = axios.create({
   baseURL: FHIR_API_URL,
   headers: {
-    Accept: 'application/fhir+json',
-    Authorization: localStorage.getItem(PRACTITIONER_ID)
+    Accept: 'application/fhir+json'
   }
 })
 
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem(ACCES_TOKEN)
-//   config.headers.Authorization = `Bearer ${token}`
-//   return config
-// })
-//
-// api.interceptors.response.use(
-//   (response) => {
-//     return response
-//   },
-//   async function (error) {
-//     if ((401 || 400) === error.response.status) {
-//       localStorage.clear()
-//       window.location = '/'
-//     }
-//
-//     const originalRequest = error.config
-//
-//     if (error.response.status === 401 && originalRequest.url.startsWith(TOKEN_URL)) {
-//       removeTokens()
-//       return Promise.reject(error)
-//     }
-//
-//     if (error.response.status === 401 && !originalRequest._retry) {
-//       originalRequest._retry = true
-//
-//       const success = await refreshToken()
-//       if (!success) {
-//         removeTokens()
-//         return Promise.reject(error)
-//       }
-//       return axios(originalRequest)
-//     }
-//     return Promise.reject(error)
-//   }
-// )
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem(PRACTITIONER_ID)
+  config.headers.Authorization = `${token}`
+  return config
+})
+
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  async function (error) {
+    if ((401 || 400) === error.response.status) {
+      localStorage.clear()
+      window.location = '/'
+    }
+
+    const originalRequest = error.config
+
+    if (error.response.status === 401) {
+      removeTokens()
+      return Promise.reject(error)
+    }
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true
+      return axios(originalRequest)
+    }
+    return Promise.reject(error)
+  }
+)
 
 export default api
