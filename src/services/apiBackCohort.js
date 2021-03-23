d69d294b4df0aff646844e08b77fc857fa3a1751
Teva RIOU <teva.riou@gmail.com>
2021-03-23 13:19:26 +0100

    fix(arkhn): rollback api back auth

--- src/services/apiBackCohort.js
+++ src/services/apiBackCohort.js
@@ -1,21 +1,18 @@
 import axios from 'axios'
-import { BACK_API_URL } from '../constants'
-import Cookies from 'js-cookie'
+import { ACCES_TOKEN, BACK_API_URL } from '../constants'
 
 const apiBackCohort = axios.create({
   baseURL: BACK_API_URL,
-  withCredentials: true,
   headers: {
-    Accept: 'application/json',
-    'X-CSRFToken': Cookies.get('csrftoken')
+    Accept: 'application/json'
   }
 })
 
-// apiBackCohort.interceptors.request.use((config) => {
-//   const token = localStorage.getItem(ACCES_TOKEN)
-//   config.headers.Authorization = `Bearer ${token}`
-//   return config
-// })
+apiBackCohort.interceptors.request.use((config) => {
+  const token = localStorage.getItem(ACCES_TOKEN)
+  config.headers.Authorization = `Bearer ${token}`
+  return config
+})
 
 apiBackCohort.interceptors.response.use(
   (response) => {
