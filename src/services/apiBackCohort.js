147cd385e7df8d05ab2bd5a8d268f170d1d49b16
Teva RIOU <teva.riou@gmail.com>
2021-03-24 17:48:37 +0100

    feat(auth): add comment on openApiBackSession

--- src/services/apiBackCohort.js
+++ src/services/apiBackCohort.js
@@ -26,6 +26,10 @@ apiBackCohort.interceptors.response.use(
 )
 
 // FIXME: remove this eventually
+// The backend auth a user at the first request and open a session
+// by creating a session cookie and a csrf token cookie.
+// We need to open a Django session before sending any useful requests
+//(otherwise we get 403 responses telling use the csrf token is missing).
 export const openApiBackSession = () => apiBackCohort.get('/')
 
 export default apiBackCohort
