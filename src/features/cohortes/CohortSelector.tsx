7042d51bf9042263ed29d54b109a6365e2b8c583
BPierrick <pierrick.bellut95@gmail.com>
2021-03-31 16:06:02 +0200

    Set "warning" html color for out of scope bars

--- src/features/cohortes/CohortSelector.tsx
+++ src/features/cohortes/CohortSelector.tsx
@@ -29,7 +29,7 @@ const cohortOrgaRepartitionDataSelector = createSelector(self, (state):
     return {
       ...data,
       value: isOrgaInScope ? data.value : Math.floor(patientsOutOfPerimeterCount / orgasOutOfPerimeterCount),
-      color: isOrgaInScope ? '#16BDFF' : '#BBBBBB'
+      color: isOrgaInScope ? '#16BDFF' : '#FFF4E5'
     }
   })
 })
