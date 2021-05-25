a8eb2cdd802bb0d451f208f8eb330cc93d7623d6
Teva RIOU <teva.riou@gmail.com>
2021-05-25 20:01:57 +0200

    fix(cohortCreation): condition form naming

--- src/components/Cohort/CreationCohort/DiagramView/components/GroupCard/components/CriteriaRightPanel/Cim10Form/components/Form/Cim10Form.tsx
+++ src/components/Cohort/CreationCohort/DiagramView/components/GroupCard/components/CriteriaRightPanel/Cim10Form/components/Form/Cim10Form.tsx
@@ -84,13 +84,13 @@ const Cim10Form: React.FC<Cim10FormProps> = (props) => {
             },
             {
               name: 'code',
-              label: 'Code CIM10',
+              label: 'Code',
               variant: 'outlined',
               type: 'autocomplete',
               multiple: true,
-              autocompleteOptions: criteria?.data?.cim10Diagnostic || [],
+              autocompleteOptions: [],
               getAutocompleteOptions: getDiagOptions,
-              noOptionsText: 'Veuillez entrer un code ou un diagnostique CIM10'
+              noOptionsText: 'Veuillez entrer un code ou un diagnostic'
             }
             // {
             //   name: 'diagnosticType',
