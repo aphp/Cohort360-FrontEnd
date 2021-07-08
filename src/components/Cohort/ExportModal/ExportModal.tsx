c94481899a2f8a1c89952c70b7990d98613c4819
vmariot <mariotvictor@gmail.com>
2021-07-08 14:16:13 +0200

    [2.3][Feat] - Link ExportModal with backend

--- src/components/Cohort/ExportModal/ExportModal.tsx
+++ src/components/Cohort/ExportModal/ExportModal.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 
+import Alert from '@material-ui/lab/Alert'
 import Button from '@material-ui/core/Button'
+import Checkbox from '@material-ui/core/Checkbox'
 import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
@@ -8,24 +10,31 @@ import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import FormGroup from '@material-ui/core/FormGroup'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Grid from '@material-ui/core/Grid'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
-import Checkbox from '@material-ui/core/Checkbox'
+import IconButton from '@material-ui/core/IconButton'
 import Switch from '@material-ui/core/Switch'
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 
+import InfoIcon from '@material-ui/icons/Info'
+
 import useStyles from './styles'
 
 import export_table from './export_table'
+import { createExport } from 'services/export'
 
 const initialState = {
   motif: '',
   conditions: false,
   tables: export_table.map(({ table_id }) => table_id)
 }
+const ERROR_MOTIF: 'ERROR_MOTIF' = 'ERROR_MOTIF'
+const ERROR_CONDITION: 'ERROR_CONDITION' = 'ERROR_CONDITION'
+const ERROR_TABLE: 'ERROR_TABLE' = 'ERROR_TABLE'
 
 type ExportModalProps = {
   cohortId: string
@@ -36,6 +45,7 @@ type ExportModalProps = {
 const ExportModal: React.FC<ExportModalProps> = ({ cohortId, open, handleClose }) => {
   const classes = useStyles()
   const [settings, setSettings] = useState(initialState)
+  const [error, setError] = useState<typeof ERROR_MOTIF | typeof ERROR_CONDITION | typeof ERROR_TABLE | null>(null)
 
   useEffect(() => {
     setSettings(initialState)
@@ -54,6 +64,7 @@ const ExportModal: React.FC<ExportModalProps> = ({ cohortId, open, handleClose }
   }
 
   const handleChangeSettings = (key: 'motif' | 'conditions' | 'tables', value: any) => {
+    setError(null)
     setSettings((prevState) => ({
       ...prevState,
       [key]: value
@@ -61,7 +72,21 @@ const ExportModal: React.FC<ExportModalProps> = ({ cohortId, open, handleClose }
   }
 
   const handleSubmit = () => {
-    console.log('settings :>> ', settings)
+    settings.motif = settings?.motif ? settings?.motif.trim() : ''
+
+    if (!settings?.motif || settings?.motif.length <= 10) {
+      return setError(ERROR_MOTIF)
+    } else if (!settings?.conditions) {
+      return setError(ERROR_CONDITION)
+    } else if (!settings?.tables || (settings?.tables && settings?.tables.length == 0)) {
+      return setError(ERROR_TABLE)
+    }
+
+    createExport({
+      cohortId,
+      motivation: settings?.motif,
+      tables: settings?.tables
+    })
     handleClose()
   }
 
@@ -72,10 +97,25 @@ const ExportModal: React.FC<ExportModalProps> = ({ cohortId, open, handleClose }
       </DialogTitle>
       <DialogContent>
         <DialogContentText>
-          Pour effectuer un export de donnée, veillez renseigner un motif, selectionner les tables que vous voulez
+          Pour effectuer un export de données, veillez renseigner un motif, selectionner les tables que vous voulez
           exporter et accepter les conditions de l'entrepôt de données (EDS). <br />
           Tous les champs sont obligatoires
         </DialogContentText>
+
+        <Grid style={{ marginBottom: 12 }}>
+          {error === ERROR_MOTIF && (
+            <Alert severity="error">
+              Merci d'indiquer le motif de votre demande d'export, ce motif doit contenir au moins 10 caractères
+            </Alert>
+          )}
+
+          {error === ERROR_CONDITION && (
+            <Alert severity="error">Merci d'accepter les conditions de l'Entrepôts de données de santé</Alert>
+          )}
+          {error === ERROR_TABLE && (
+            <Alert severity="error">Merci d'indiquer les tables que vous voulez exporter</Alert>
+          )}
+        </Grid>
 
         <FormGroup>
           <TextField
@@ -91,9 +131,15 @@ const ExportModal: React.FC<ExportModalProps> = ({ cohortId, open, handleClose }
             onChange={(e) => handleChangeSettings('motif', e.target.value)}
           />
 
-          <Typography className={classes.tableTitle} variant="h6">
-            Tables exportées
-          </Typography>
+          <Grid container justify="space-between" alignItems="center">
+            <Typography className={classes.tableTitle} variant="h6">
+              Tables exportées
+            </Typography>
+
+            <IconButton size="small" onClick={() => window.open(`https://doc.eds.aphp.fr/omop/tables`, '_blank')}>
+              <InfoIcon />
+            </IconButton>
+          </Grid>
 
           <List className={classes.list}>
             <ListItem className={classes.tableListElement}>
@@ -113,7 +159,7 @@ const ExportModal: React.FC<ExportModalProps> = ({ cohortId, open, handleClose }
             </ListItem>
             {export_table.map(({ table_name, table_id }) => (
               <ListItem className={classes.tableListElement} key={table_id}>
-                <ListItemText primary={table_name} secondary={table_id} />
+                <ListItemText primary={table_name} />
                 <ListItemSecondaryAction>
                   <Checkbox
                     checked={!!settings.tables.find((tableId) => tableId === table_id)}
@@ -133,7 +179,11 @@ const ExportModal: React.FC<ExportModalProps> = ({ cohortId, open, handleClose }
               />
             }
             labelPlacement="start"
-            label="Je reconnais avoir lu et j'accepte les conditions de l'EDS"
+            label={
+              <Typography>
+                Je reconnais avoir lu et j'accepte les <a href={'/toto'}>conditions de l'EDS</a>
+              </Typography>
+            }
           />
         </FormGroup>
       </DialogContent>
