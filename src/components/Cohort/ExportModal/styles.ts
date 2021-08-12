58299bcd7697c1356992bbbae2ba0e5e13b1ee95
vmariot <mariotvictor@gmail.com>
2021-08-12 15:52:03 +0200

    [2.4][Feat] - Change design of ExportModal

--- src/components/Cohort/ExportModal/styles.ts
+++ src/components/Cohort/ExportModal/styles.ts
@@ -5,11 +5,24 @@ const useStyles = makeStyles((theme) => ({
   list: {
     border: `1px solid ${theme.palette.grey[400]}`,
     borderRadius: 4,
-    maxHeight: 450,
+    maxHeight: 300,
     minHeight: 200,
     overflow: 'auto'
   },
-  tableListElement: {}
+  tableListElement: {},
+  conditionItem: {
+    margin: '4px 0 4px 36px',
+    position: 'relative',
+    '&::before': {
+      position: 'absolute',
+      content: "'•'",
+      color: '#19235A',
+      left: -20,
+      top: 'calc(50% - 11px)',
+      fontSize: 26,
+      lineHeight: '22px'
+    }
+  }
 }))
 
 export default useStyles
