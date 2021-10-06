73c133e3555ceb369d241b05e3e9699744f22bcc
vmariot <mariotvictor@gmail.com>
2021-10-06 15:50:17 +0200

    [2.6][Fix] - Error while searching in documents in the requeteur

--- src/utils/cohortCreation.ts
+++ src/utils/cohortCreation.ts
@@ -294,6 +294,7 @@ const constructFilterFhir = (criterion: SelectedCriteriaType) => {
         `${
           criterion.search
             ? `${COMPOSITION_TEXT}=${criterion.search
+                .trim()
                 .replaceAll('!', '%21')
                 .replaceAll('#', '%23')
                 .replaceAll('$', '%24')
@@ -309,9 +310,13 @@ const constructFilterFhir = (criterion: SelectedCriteriaType) => {
                 .replaceAll(';', '%3B')
                 .replaceAll('=', '%3D')
                 .replaceAll('?', '%3F')
+                .replaceAll('\n', ' ')
+                .replaceAll('  ', ' ')
+                .replaceAll(' ', '%20')
                 .replaceAll('@', '%40')
                 .replaceAll('[', '%5B')
-                .replaceAll(']', '%5D')}`
+                .replaceAll(']', '%5D')
+                .replaceAll('|', '%7C')}`
             : ''
         }`,
         `${
@@ -873,6 +878,28 @@ export async function unbuildRequest(_json: string) {
             switch (key) {
               case COMPOSITION_TEXT:
                 currentCriterion.search = value
+                  ? value
+                      .replaceAll('%21', '!')
+                      .replaceAll('%23', '#')
+                      .replaceAll('%24', '$')
+                      .replaceAll('%26', '&')
+                      .replaceAll('%27', "'")
+                      .replaceAll('%28', '(')
+                      .replaceAll('%29', ')')
+                      .replaceAll('%2A', '*')
+                      .replaceAll('%2B', '+')
+                      .replaceAll('%2C', ',')
+                      .replaceAll('%2F', '/')
+                      .replaceAll('%3A', ':')
+                      .replaceAll('%3B', ';')
+                      .replaceAll('%3D', '=')
+                      .replaceAll('%3F', '?')
+                      .replaceAll('%20', ' ')
+                      .replaceAll('%40', '@')
+                      .replaceAll('%5B', '[')
+                      .replaceAll('%5D', ']')
+                      .replaceAll('%7C', '|')
+                  : null
                 break
               case COMPOSITION_TYPE: {
                 const docTypeIds = value?.split(',')
