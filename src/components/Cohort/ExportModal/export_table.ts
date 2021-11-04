6932cd214d3cf8c54bef15e8f5b74b15bef11824
vmariot <mariotvictor@gmail.com>
2021-11-04 16:32:42 +0100

    [2.7][Feat] - Remove `drug_exposure`

--- src/components/Cohort/ExportModal/export_table.ts
+++ src/components/Cohort/ExportModal/export_table.ts
@@ -16,11 +16,6 @@ const exportTable: ExportTableType[] = [
     table_id: 'observation'
   },
   {
-    id: 'drug_exposure',
-    table_name: 'Patient - Médicaments',
-    table_id: 'drug_exposure'
-  },
-  {
     id: 'drug_exposure_prescription',
     table_name: 'Patient - Médicaments - Prescription',
     table_id: 'drug_exposure_prescription'
