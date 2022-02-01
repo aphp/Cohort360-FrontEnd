a1c21cb41f342e64d53dba49a716746761499ab0
bplaye <benoit.playe-ext@aphp.fr>
2022-02-01 16:55:18 +0100

    [2.10][fix] WIP : init test selenium : rm some log

--- .gitlab/test.sh
+++ .gitlab/test.sh
@@ -3,17 +3,10 @@ npm install
 npm run start > file.log 2>&1 &
 sleep 30
 
-echo "1"
-ls /etc/
-echo "2"
-ls /etc/nginx
-echo "3"
-ls /etc/nginx/conf.d/
-echo "4"
 cp .gitlab/nginx-test.conf /etc/nginx/conf.d/nginx.conf
-echo /etc/nginx/conf.d/nginx.conf
+cat /etc/nginx/conf.d/nginx.conf
 export $(cat $(printenv | grep -E "\b""$CI_COMMIT_BRANCH""_ENV""\b" | cut -d '=' -f 2-) | xargs)
 bash docker/entry-point.sh
-echo /etc/nginx/conf.d/nginx.conf
+cat /etc/nginx/conf.d/nginx.conf
 nginx
-
+echo "done"
