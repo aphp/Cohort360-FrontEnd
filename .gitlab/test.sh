c247758ee7cee684541f2ac817019d70a6b2c272
bplaye <benoit.playe-ext@aphp.fr>
2022-02-01 16:19:29 +0100

    [2.10][fix] WIP : init test selenium

---
+++ .gitlab/test.sh
@@ -0,0 +1,11 @@
+npm config set proxy http://$PROXY_URL
+npm install
+npm run start > file.log 2>&1 &
+sleep 30
+
+cp .gitlab/nginx-test.conf /etc/nginx/conf.d/nginx.conf
+export $(cat $(printenv | grep -E "\b""$CI_COMMIT_BRANCH""_ENV""\b" | cut -d '=' -f 2-) | xargs)
+bash docker/entry-point.sh
+nginx
+
+npm run test
