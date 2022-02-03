948478e8da8a4100b00630ac0b19078ccde86f15
Salah-BOUYAHIA <salah.bouyahia-ext@aphp.fr>
2022-02-03 10:56:06 +0100

    [2.10][fix] - fix k8s CICD for selenium test

--- .gitlab/test.sh
+++ .gitlab/test.sh
@@ -1,12 +1,9 @@
+cp .gitlab/nginx-test.conf /etc/nginx/conf.d/nginx.conf
+export $(cat $(printenv | grep -E "\b""$CI_COMMIT_BRANCH""_ENV""\b" | cut -d '=' -f 2-) | xargs)
+export $(cat $(printenv | grep -E "\b""$CI_COMMIT_BRANCH""_build_ENV""\b" | cut -d '=' -f 2-) | xargs)
+bash docker/entry-point.sh
+service nginx restart
 npm config set proxy http://$PROXY_URL
 npm install
 npm run start > file.log 2>&1 &
 sleep 30
-
-cp .gitlab/nginx-test.conf /etc/nginx/conf.d/nginx.conf
-cat /etc/nginx/conf.d/nginx.conf
-export $(cat $(printenv | grep -E "\b""$CI_COMMIT_BRANCH""_ENV""\b" | cut -d '=' -f 2-) | xargs)
-bash docker/entry-point.sh
-cat /etc/nginx/conf.d/nginx.conf
-service nginx restart
-echo "done"
