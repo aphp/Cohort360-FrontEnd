a6a493f28a81801d9758422f029de96ea4c26ec4
bplaye <benoit.playe-ext@aphp.fr>
2022-02-07 12:07:40 +0100

    [2.11][fix] - debug CMD in Dockerfile

--- .gitlab/test.sh
+++ .gitlab/test.sh
@@ -4,9 +4,6 @@ npm run start > file.log 2>&1 &
 sleep 30
 
 cp .gitlab/nginx-test.conf /etc/nginx/conf.d/nginx.conf
-cat /etc/nginx/conf.d/nginx.conf
 export $(cat $(printenv | grep -E "\b""$CI_COMMIT_BRANCH""_ENV""\b" | cut -d '=' -f 2-) | xargs)
-bash docker/entry-point.sh
-cat /etc/nginx/conf.d/nginx.conf
+bash .gitlab/nginx-rename.sh
 service nginx restart
-echo "done"
--- docker/entry-point.sh
+++ docker/entry-point.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/bash
 
-declare -a UrlArray=("JWT_URL" "BACK_URL" "FHIR_URL" "DISPOSE_URL" "PORTAIL_URL")
-
-for NAME in ${UrlArray[@]}; do
-  ENV_VAR="${!NAME}"
-  ENV_VAR=$(echo $ENV_VAR | sed "s/[^a-zA-Z0-9]/\\\\&/g");
-  sed -i s/"{{"$NAME"}}"/$ENV_VAR/g /etc/nginx/conf.d/nginx.conf;
-done
-
+bash .gitlab/nginx-rename.sh
 service nginx restart
+sleep infinity
