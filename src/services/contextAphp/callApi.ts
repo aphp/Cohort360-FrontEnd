70a8345ad039b848bdb4711207b95df33a5df61b
manelleg <manelle.gueriouz@aphp.fr>
2022-03-28 12:54:13 +0200

    [2.13][fix] - added filters in fetchObservation to filter non numerical data

--- src/services/contextAphp/callApi.ts
+++ src/services/contextAphp/callApi.ts
@@ -249,7 +249,7 @@ export const fetchComposition = async (args: fetchCompositionProps) => {
   uniqueFacet = uniqueFacet ? uniqueFacet.filter(uniq) : []
   _elements = _elements ? _elements.filter(uniq) : []
 
-  // By default, all the calls to `/Composition` will have `type:not=doc-impor in parameter
+  // By default, all the calls to `/Composition` will have `type:not=doc-impor` in parameter
   let options: string[] = ['type:not=doc-impor', 'empty=false']
   if (_id)                                         options = [...options, `_id=${_id}`]                                                         // eslint-disable-line
   if (size !== undefined)                          options = [...options, `size=${size}`]                                                       // eslint-disable-line
@@ -523,7 +523,8 @@ export const fetchObservation = async (args: fetchObservationProps) => {
 
   _list = _list ? _list.filter(uniq) : []
 
-  let options: string[] = []
+  // By default, all the calls to `/Observation` will have 'value-quantity-value=ge0,le0' in the parameters
+  let options: string[] = ['value-quantity-value=ge0,le0']
   if (id)                                           options = [...options, `id=${id}`]                                                                 // eslint-disable-line
   if (size !== undefined)                           options = [...options, `size=${size}`]                                                             // eslint-disable-line
   if (offset)                                       options = [...options, `offset=${offset}`]                                                         // eslint-disable-line
