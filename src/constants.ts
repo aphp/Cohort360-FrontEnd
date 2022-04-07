c3a1cd5a17095a7b2ec1106aafb727363575368e
Salah-BOUYAHIA <salah.bouyahia-ext@aphp.fr>
2022-04-07 15:24:45 +0200

    [2.13][fix] - remove {CONTEXT} into constant.ts

--- src/constants.ts
+++ src/constants.ts
@@ -23,8 +23,8 @@ export const CONTEXT =
   process.env.NODE_ENV !== 'development' ? '{CONTEXT}' : (process.env.REACT_APP_CONTEXT as ContextType)
 
 if (!CONTEXT) throw new Error('missing REACT_APP_CONTEXT from environment')
-if (CONTEXT !== 'arkhn' && CONTEXT !== 'aphp' && CONTEXT !== 'fakedata' && CONTEXT !== '{CONTEXT}') {
-  throw new Error("REACT_APP_CONTEXT must be either 'aphp', 'fakedata' or 'arkhn and context is" + CONTEXT)
+if (CONTEXT !== 'arkhn' && CONTEXT !== 'aphp' && CONTEXT !== 'fakedata') {
+  throw new Error("REACT_APP_CONTEXT must be either 'aphp', 'fakedata' or 'arkhn and context is {" + CONTEXT + '}')
 }
 export const ACCES_TOKEN = 'access'
 export const REFRESH_TOKEN = 'refresh'
