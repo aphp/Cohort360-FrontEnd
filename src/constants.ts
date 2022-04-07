52497729e3d6711cdd582bc43c00532544f5b0a4
Salah-BOUYAHIA <salah.bouyahia-ext@aphp.fr>
2022-04-07 16:08:14 +0200

    [2.13][fix] - comment the context error condition

--- src/constants.ts
+++ src/constants.ts
@@ -22,10 +22,10 @@ export const LOGIN_REDIRECT_URL = 'irrelevant'
 export const CONTEXT =
   process.env.NODE_ENV !== 'development' ? '{CONTEXT}' : (process.env.REACT_APP_CONTEXT as ContextType)
 
-if (!CONTEXT) throw new Error('missing REACT_APP_CONTEXT from environment')
-if (CONTEXT != 'arkhn' && CONTEXT != 'aphp' && CONTEXT != 'fakedata') {
-  throw new Error("REACT_APP_CONTEXT must be either 'aphp', 'fakedata' or 'arkhn and context is {" + CONTEXT + '}')
-}
+// if (!CONTEXT) throw new Error('missing REACT_APP_CONTEXT from environment')
+// if (CONTEXT != 'arkhn' && CONTEXT != 'aphp' && CONTEXT != 'fakedata') {
+//   throw new Error("REACT_APP_CONTEXT must be either 'aphp', 'fakedata' or 'arkhn and context is {" + CONTEXT + '}')
+// }
 export const ACCES_TOKEN = 'access'
 export const REFRESH_TOKEN = 'refresh'
 
