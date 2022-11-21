67cc20b299fd39e21aa18d3f87830eb6ec46cada
Salah-BOUYAHIA <salah.bouyahia-ext@aphp.fr>
2022-11-21 11:31:26 +0100

    [2.18.1][hot-fix] - corretion of spelling mistakes - Ref gestion-de-projet#1692

--- src/components/Welcome/NewsCard/News/SixthNews.tsx
+++ src/components/Welcome/NewsCard/News/SixthNews.tsx
@@ -9,7 +9,7 @@ const FifthNews = () => {
         <b>Nouvelles fonctionnalités</b> - 26/10/2022
       </Typography>
       <Typography color="textSecondary" style={{ paddingBottom: 8, paddingLeft: 16 }}>
-        • <b>La liste d'IPP</b> à été rajouté à la liste de critère dans le requêteur.
+        • <b>La liste d'IPP</b> à été rajoutée à la liste de critère dans le requêteur.
       </Typography>
       <Typography color="textSecondary" style={{ paddingBottom: 8, paddingLeft: 16 }}>
         • Il est possible d'afficher des éléments <b>ANABIO</b> associés au code <b>LOINC</b>.
@@ -22,7 +22,7 @@ const FifthNews = () => {
         • <b>Une page 404</b> à été ajoutée quand on se rend sur une page inexistante.
       </Typography>
       <Typography color="textSecondary" style={{ paddingBottom: 8, paddingLeft: 16 }}>
-        • Diverses corrections ont étés apportées <b>aux filtres</b> ainsi que sur <b>les tris</b>.
+        • Diverses corrections ont été apportées <b>aux filtres</b> ainsi que sur <b>les tris</b>.
       </Typography>
       <Typography color="textSecondary" style={{ paddingTop: 6 }}>
         <b>L'équipe Cohort360</b>
