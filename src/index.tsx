dc936e8b8c9ea472db3cff04ff2bf4e16223a928
Paul Bui-Quang <paul.bui-quang@aphp.fr>
2023-05-04 17:04:11 +0200

    refactor(theme): extract theme into a separate file

--- src/index.tsx
+++ src/index.tsx
@@ -1,93 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createTheme, ThemeProvider, Theme, StyledEngineProvider } from '@mui/material'
+import { ThemeProvider, StyledEngineProvider } from '@mui/material'
 
 import App from './App'
-
-declare module '@mui/styles/defaultTheme' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends Theme {}
-}
-
-const theme = createTheme({
-  palette: {
-    common: {
-      black: '#153D8A'
-    },
-    primary: {
-      main: '#0063AF'
-    },
-    secondary: {
-      main: '#ED6D91'
-    },
-    action: {
-      active: '#5BC5F2'
-    },
-    text: {
-      primary: '#153D8A'
-    },
-    background: {
-      default: '#E6F1FD'
-    }
-  },
-  typography: {
-    fontFamily: "'Open Sans', sans-serif",
-    h1: {
-      fontFamily: "'Montserrat', sans-serif",
-      fontSize: '32px',
-      fontWeight: 'bold'
-    },
-    h2: {
-      fontFamily: "'Montserrat', sans-serif",
-      fontSize: '18px',
-      fontWeight: 'bold'
-    },
-    h3: {
-      fontFamily: "'Montserrat', sans-serif",
-      fontSize: '13px',
-      fontWeight: 'bold'
-    },
-    h4: {
-      fontSize: '32px',
-      fontWeight: 'bold'
-    },
-    h5: {
-      fontSize: '16px',
-      fontWeight: 'bold'
-    },
-    h6: {
-      fontSize: '11px',
-      fontWeight: 'bold',
-      textTransform: 'uppercase',
-      lineHeight: '2.66'
-    },
-    body1: {
-      fontSize: '13px'
-    },
-    body2: {
-      fontSize: '14px'
-    },
-    button: {
-      fontFamily: "'Open Sans', sans-serif",
-      fontWeight: 'bold',
-      fontSize: '15px',
-      textTransform: 'none'
-    }
-  },
-  components: {
-    MuiDialogTitle: {
-      styleOverrides: {
-        root: {
-          fontSize: '18px',
-          fontFamily: "'Montserrat', sans-serif",
-          color: '#0063AF',
-          textTransform: 'none',
-          lineHeight: 2
-        }
-      }
-    }
-  }
-})
+import theme from 'theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
