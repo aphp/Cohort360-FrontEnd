739f9dee08bbe6bbd7aa766e5660416b7fd54b99
Salah-BOUYAHIA <salah.bouyahia-ext@aphp.fr>
2023-06-02 12:24:11 +0200

    refactor: starting creation of new caresite selection flow - Ref gestion-de-projet#2027

---
+++ src/components/ScopeTree/NewScopeTree/ChipsMultiSelection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ChipsMultiSelection = (props: any) => {
+  return (
+    <div>
+      je retourne des chips avec les caresite selectionne{props} lors d'une recherche et j'ai la possibilite de les
+      supprimer
+    </div>
+  )
+}
+
+export default ChipsMultiSelection
--- src/components/ScopeTree/NewScopeTree/NewScopeTreeComponent.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const NewScopeTreeComponent = () => {
-  return <div>NewScopeTreeComponent</div>
-}
-
-export default NewScopeTreeComponent
