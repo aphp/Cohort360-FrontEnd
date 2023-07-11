a88ed0633ab296f3e5f2cd0bf73c9562ce966278
itsEAF <126916158+itsEAF@users.noreply.github.com>
2023-07-11 15:40:54 +0200

    feat: add view textual pdf not available - Ref gestion-de-projet#1390
    * feat: wip modal with pdf or text raw - Ref gestion-de-projet#1390
    
    * feat: wip modal with pdf or text raw - Ref gestion-de-projet#1390
    
    * feat: add feat possibilty to view content textual when pdf not available - Ref gestion-de-projet#1390

--- src/components/DocumentViewer/DocumentViewer.tsx
+++ src/components/DocumentViewer/DocumentViewer.tsx
@@ -7,7 +7,6 @@ import CircularProgress from '@mui/material/CircularProgress'
 import Dialog from '@mui/material/Dialog'
 import DialogActions from '@mui/material/DialogActions'
 import DialogContent from '@mui/material/DialogContent'
-import DialogTitle from '@mui/material/DialogTitle'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 
@@ -18,6 +17,7 @@ import services from 'services/aphp'
 import Watermark from 'assets/images/watermark_pseudo.svg'
 import { DocumentReference } from 'fhir/r4'
 import { getAuthorizationMethod } from 'services/apiFhir'
+import { Tabs, Tab } from '@mui/material'
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
@@ -33,6 +33,11 @@ const DocumentViewer: React.FC<DocumentViewerProps> = ({ deidentified, open, han
   const [documentContent, setDocumentContent] = useState<DocumentReference | null>(null)
   const [numPages, setNumPages] = useState(1)
   const [loading, setLoading] = useState(false)
+  const [selectedTab, setSelectedValue] = useState<'pdf' | 'raw'>(!deidentified ? (documentId ? 'pdf' : 'raw') : 'raw')
+
+  const handleTabChange = (event: React.SyntheticEvent, newValue: 'pdf' | 'raw') => {
+    setSelectedValue(newValue)
+  }
 
   useEffect(() => {
     const _fetchDocumentContent = async () => {
@@ -46,9 +51,7 @@ const DocumentViewer: React.FC<DocumentViewerProps> = ({ deidentified, open, han
       setLoading(false)
     }
 
-    if (deidentified) {
-      _fetchDocumentContent()
-    }
+    _fetchDocumentContent()
 
     return () => {
       setDocumentContent(null)
@@ -62,53 +65,68 @@ const DocumentViewer: React.FC<DocumentViewerProps> = ({ deidentified, open, han
     margin: 'auto'
   }
 
-  const documentContentDecode = documentContent?.content?.[1].attachment?.data
-    ? Buffer.from(documentContent.content[1].attachment.data, 'base64').toString('utf-8')
-    : ''
+  const findContent = documentContent?.content?.find(
+    (content) => content.attachment?.contentType === 'Document Data (Base64 Encoded)'
+  )
+
+  const documentContentDecode =
+    findContent?.attachment.data != undefined
+      ? Buffer.from(findContent.attachment.data, 'base64').toString('utf-8')
+      : ''
 
   return (
     <Dialog open={open} fullWidth maxWidth="xl" onClose={handleClose}>
-      <DialogTitle id="document-viewer-dialog-title"></DialogTitle>
       <DialogContent id="document-viewer-dialog-content">
-        {deidentified ? (
-          loading ? (
-            <DialogContent id="document-viewer-dialog-content">
-              <CircularProgress />
-            </DialogContent>
-          ) : (
-            <div style={{ backgroundImage: `url(${Watermark})` }}>
-              {documentContentDecode ? (
-                <Typography>{ReactHtmlParser(documentContentDecode)}</Typography>
-              ) : (
-                <Typography>Le contenu du document est introuvable.</Typography>
-              )}
-            </div>
-          )
+        {loading ? (
+          <DialogContent id="document-viewer-dialog-content">
+            <CircularProgress />
+          </DialogContent>
         ) : (
-          <Grid style={pdfViewerContainerStyle}>
-            <Document
-              error={'Le document est introuvable.'}
-              loading={'PDF en cours de chargement...'}
-              file={{
-                url: `${FHIR_API_URL}/Binary/${documentId}`,
-                httpHeaders: {
-                  Accept: 'application/pdf',
-                  Authorization: `Bearer ${localStorage.getItem('access')}`,
-                  authorizationMethod: getAuthorizationMethod()
-                }
-              }}
-              onLoadSuccess={({ numPages }) => setNumPages(numPages)}
-            >
-              {Array.from(new Array(numPages), (el, index) => (
-                <Page
-                  width={window.innerWidth * 0.9}
-                  key={`page_${index + 1}`}
-                  pageNumber={index + 1}
-                  loading={'Pages en cours de chargement...'}
-                />
-              ))}
-            </Document>
-          </Grid>
+          <>
+            <Tabs value={selectedTab} onChange={handleTabChange}>
+              {!deidentified && <Tab label="PDF" value="pdf" />}
+              <Tab label="Fichier brut" value="raw" />
+            </Tabs>
+            <>
+              {selectedTab === 'raw' && (
+                <div style={{ backgroundImage: `url(${Watermark})` }}>
+                  {documentContentDecode ? (
+                    <Typography>{ReactHtmlParser(documentContentDecode)}</Typography>
+                  ) : (
+                    <Typography>Le contenu du document est introuvable.</Typography>
+                  )}
+                </div>
+              )}
+            </>
+            <>
+              {selectedTab === 'pdf' && (
+                <Grid style={pdfViewerContainerStyle}>
+                  <Document
+                    error={'Le document est introuvable.'}
+                    loading={'PDF en cours de chargement...'}
+                    file={{
+                      url: `${FHIR_API_URL}/Binary/${documentId}`,
+                      httpHeaders: {
+                        Accept: 'application/pdf',
+                        Authorization: `Bearer ${localStorage.getItem('access')}`,
+                        authorizationMethod: getAuthorizationMethod()
+                      }
+                    }}
+                    onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+                  >
+                    {Array.from(new Array(numPages), (el, index) => (
+                      <Page
+                        width={window.innerWidth * 0.9}
+                        key={`page_${index + 1}`}
+                        pageNumber={index + 1}
+                        loading={'Pages en cours de chargement...'}
+                      />
+                    ))}
+                  </Document>
+                </Grid>
+              )}
+            </>
+          </>
         )}
       </DialogContent>
       <DialogActions>
