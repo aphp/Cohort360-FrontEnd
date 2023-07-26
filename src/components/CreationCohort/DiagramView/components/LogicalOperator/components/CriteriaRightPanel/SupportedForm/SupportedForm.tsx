1fd98582cf2d2b15d18b926dc373ef8227d9a114
Salah-BOUYAHIA <salah.bouyahia-ext@aphp.fr>
2023-07-26 12:04:50 +0200

    fix: resolve confirm supported criteria with just an encounterService

--- src/components/CreationCohort/DiagramView/components/LogicalOperator/components/CriteriaRightPanel/SupportedForm/SupportedForm.tsx
+++ src/components/CreationCohort/DiagramView/components/LogicalOperator/components/CriteriaRightPanel/SupportedForm/SupportedForm.tsx
@@ -105,6 +105,7 @@ const SupportedForm: React.FC<SupportedFormProps> = (props) => {
       defaultValues.destination?.length === 0 &&
       defaultValues.provenance?.length === 0 &&
       defaultValues.admission?.length === 0 &&
+      defaultValues.encounterService?.length === 0 &&
       !defaultValues.encounterStartDate &&
       !defaultValues.encounterEndDate
     ) {
@@ -128,6 +129,7 @@ const SupportedForm: React.FC<SupportedFormProps> = (props) => {
   }
 
   const _onSubmit = () => {
+    console.log('defaultValues', defaultValues)
     const errorType = onCheckFormError()
     setError(errorType)
     if (errorType === Error.NO_ERROR) onChangeSelectedCriteria(defaultValues)
