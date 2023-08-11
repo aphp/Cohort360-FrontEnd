8b628cc30f4e727850c84f027b45d19028477874
mourads <mourad.sellam@open-groupe.com>
2023-08-11 18:04:29 +0200

    refactor: set scope tree tabs (3) - Ref gestion-de-projet#2027

---
+++ src/components/NewScopeTree/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from 'tss-react/mui'
+
+const useStyles = makeStyles()(() => ({
+  tabs: {
+    marginTop: 72,
+    position: 'absolute',
+    width: '100%'
+  }
+}))
+
+export default useStyles
