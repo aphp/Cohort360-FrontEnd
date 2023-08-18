d10838a1bddaf6b479e13a3f8092dca7e8ebd7e6
manelleg <manelle.gueriouz@aphp.fr>
2023-08-18 17:21:49 +0200

    style: finished partial temporal constraints style - Ref gestion-de-projet#2033

--- src/components/CreationCohort/DiagramView/components/TemporalConstraintCard/components/TemporalConstraintModal/styles.ts
+++ src/components/CreationCohort/DiagramView/components/TemporalConstraintCard/components/TemporalConstraintModal/styles.ts
@@ -4,6 +4,22 @@ const useStyles = makeStyles()(() => ({
   flexBaseline: {
     display: 'flex !important',
     marginTop: '8px !important'
+  },
+  selectGroupFormControl: {
+    margin: '0 8px',
+    minWidth: 200,
+    height: '25%',
+    flexDirection: 'row',
+    alignItems: 'baseline'
+  },
+  selectCriteriaFormControl: {
+    display: 'flex',
+    height: '75%',
+    flexWrap: 'wrap',
+    overflow: 'scroll',
+    width: '100%',
+    flexDirection: 'row',
+    margin: '8px 0 0'
   }
 }))
 
