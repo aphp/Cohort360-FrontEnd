7656f606413de7eb1f098a968d412d625b5a4cd4
Paul Bui-Quang <paul.bui-quang@aphp.fr>
2023-10-12 17:26:38 +0200

    fix(medication): add fallback codesystem code display when standard is missing
    * fix(medication): add fallback codesystem code display when standard is missing
    
    * fix(medication): add fallback codesystem code display when standard is missing
    
    ---------
    
    Co-authored-by: Salah-BOUYAHIA <salah.bouyahia-ext@aphp.fr>

--- src/constants.js
+++ src/constants.js
@@ -84,6 +84,10 @@ export const ENCOUNTER_ADMISSION = import.meta.env.DEV
   ? import.meta.env.VITE_VALUE_SET_URL_ENCOUNTER_ADMISSION
   : '{VITE_VALUE_SET_URL_ENCOUNTER_ADMISSION}'
 
+export const MEDICATION_ATC_ORBIS = import.meta.env.DEV
+  ? import.meta.env.VITE_MEDICATION_ATC_ORBIS
+  : '{VITE_MEDICATION_ATC_ORBIS}'
+
 export const MEDICATION_ATC = import.meta.env.DEV
   ? import.meta.env.VITE_VALUE_SET_URL_MEDICATION_ATC
   : '{VITE_VALUE_SET_URL_MEDICATION_ATC}'
