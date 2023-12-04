ba5a8d14158a28cb2a4de1857fab93b5f516f008
manelleg <manelle.gueriouz@aphp.fr>
2023-12-04 16:57:21 +0100

    fix: removed alerts in medication and ccam - Ref gestion-de-projet#2406

--- src/components/CreationCohort/DiagramView/components/LogicalOperator/components/CriteriaRightPanel/MedicationForm/components/Form/MedicationForm.tsx
+++ src/components/CreationCohort/DiagramView/components/LogicalOperator/components/CriteriaRightPanel/MedicationForm/components/Form/MedicationForm.tsx
@@ -9,7 +9,6 @@ import {
   FormLabel,
   Grid,
   IconButton,
-  Link,
   Radio,
   RadioGroup,
   Switch,
@@ -126,18 +125,6 @@ const MedicationForm: React.FC<MedicationFormProps> = (props) => {
             Tous les éléments des champs multiples sont liés par une contrainte OU
           </Alert>
         )}
-
-        <Alert severity="warning">
-          Données actuellement disponibles : Circuit du médicament ORBIS. Pour plus d'informations sur les prochaines
-          intégrations de données, veuillez vous référer au tableau trimestriel de disponibilité des données disponible{' '}
-          <Link
-            href="https://eds.aphp.fr/sites/default/files/2023-01/EDS_Disponibilite_donnees_site_EDS_202212.pdf"
-            target="_blank"
-            rel="noopener"
-          >
-            ici
-          </Link>
-        </Alert>
 
         <Grid className={classes.inputContainer} container>
           <Typography variant="h6">Médicaments</Typography>
