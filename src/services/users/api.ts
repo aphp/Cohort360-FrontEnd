dd2b4b1fa29df30944ad51003802f8b87f53a627
aetchego <alizeetche@gmail.com>
2024-02-26 14:47:05 +0100

    correc refacto

--- src/services/users/api.ts
+++ src/services/users/api.ts
@@ -1,16 +1,11 @@
-import { getProviders } from 'services/aphp/serviceProviders'
+import { getUsers } from 'services/aphp/serviceUsers'
 import { Direction, LabelObject, Order } from 'types/searchCriterias'
 
 export const fetchUsers = async (searchInput?: string, signal?: AbortSignal): Promise<LabelObject[]> => {
   try {
-    const { providers } = await getProviders(
-      { orderBy: Order.LASTNAME, orderDirection: Direction.ASC },
-      1,
-      searchInput,
-      signal
-    )
-    return providers.map((elem) => {
-      return { label: `${elem.displayed_name} - ${elem.email}`, id: elem.provider_id }
+    const { users } = await getUsers({ orderBy: Order.LASTNAME, orderDirection: Direction.ASC }, 1, searchInput, signal)
+    return users.map((elem) => {
+      return { label: `${elem.display_name} - ${elem.email}`, id: elem.username || '' }
     })
   } catch (error) {
     console.error(error)
