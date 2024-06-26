95f749184aba3c2f37dc0fb978de901185975d45
Paul Bui-Quang <paul.bui-quang@aphp.fr>
2024-06-26 14:01:49 +0200

    perf(location): add viewbox limited loading + cache already loaded areas

--- src/components/Dashboard/Preview/LocationMap/index.tsx
+++ src/components/Dashboard/Preview/LocationMap/index.tsx
@@ -1,129 +1,348 @@
 import React, { useCallback, useEffect, useState } from 'react'
-import L, { LatLngTuple } from 'leaflet'
+import L, { LatLngBounds, LatLngTuple } from 'leaflet'
 // https://github.com/PaulLeCam/react-leaflet/issues/1077
 //@ts-ignore
-import { Polygon, Tooltip } from 'react-leaflet'
+import { Polygon, Rectangle, Tooltip, useMapEvents } from 'react-leaflet'
 //@ts-ignore
 import { MapContainer } from 'react-leaflet/MapContainer'
 //@ts-ignore
 import { TileLayer } from 'react-leaflet/TileLayer'
 import { fetchLocation } from 'services/aphp/callApi'
 import { getAllResults } from 'utils/apiHelpers'
-import { parseShape } from './utils'
+import { colorize, computeNearFilter, explodeBoundsIntoMesh, isBoundCovered, parseShape } from './utils'
 import { cancelPendingRequest } from 'utils/abortController'
 import * as d3 from 'd3'
-import { CircularProgress } from '@mui/material'
+import { CircularProgress, Slider, Typography } from '@mui/material'
+import CircularProgressWithLabel from 'components/ui/CircularProgressWithLabel'
 
-const LOCATION_FETCH_BATCH_SIZE = 1000
+const DEBUG_SHOW_LOADED_BOUNDS = false
+const SHOW_OPACITY_CONTROL = false
+const MAX_COUNT_DEFAULT = 1
+const LOCATION_FETCH_BATCH_SIZE = 2000
 const MAX_COUNT_QUANTILE = 0.96
-const ZONE_COLOR_OPACITY = 0.75
-const DEFAULT_MAP_CENTER = [48.8575, 2.3514]
+const ZONE_COLOR_OPACITY = 0.37
+const BORDER_RELATIVE_OPACITY = 1.33
+const DEFAULT_MAP_CENTER: LatLngTuple = [48.8575, 2.3514]
 const TILE_URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 const MAP_COPYRIGHTS = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 const LOCATION_SHAPE_EXTENSION_URL = 'https://terminology.eds.aphp.fr/fhir/profile/location/extension/position'
 const LOCATION_COUNT_EXTENSION_URL = 'https://terminology.eds.aphp.fr/fhir/profile/location/extension/count'
+const COLOR_PALETTE = [
+  '#fad370',
+  '#fac55c',
+  '#fab749',
+  '#faa837',
+  '#fb9725',
+  '#fc8613',
+  '#fd7300',
+  '#fe5c00',
+  '#ff4000',
+  '#ff0000'
+]
 
 type LocationMapProps = {
   cohortId: string
   center?: LatLngTuple
 }
 
-const LocationMap = (props: LocationMapProps) => {
-  const { cohortId, center = DEFAULT_MAP_CENTER } = props
+type IrisZonesProps = {
+  cohortId: string
+}
+
+const IrisZones = (props: IrisZonesProps) => {
+  const { cohortId } = props
   const [dataLoading, setDataLoading] = useState(true)
+  const [dataLoadingProgress, setDataLoadingProgress] = useState(0)
   const [zones, setZones] = useState<Array<{ shape: LatLngTuple[]; meta: { count: number; name: string } }>>([])
-  const [maxCount, setMaxCount] = useState(1)
+  const [visibleZones, setVisibleZones] = useState<
+    Array<{ shape: LatLngTuple[]; meta: { count: number; name: string } }>
+  >([])
+  const [bounds, setBounds] = useState<LatLngBounds | null>(null)
+  const [loadedBounds, setLoadedBounds] = useState<LatLngBounds[]>([])
+  const [maxCount, setMaxCount] = useState(MAX_COUNT_DEFAULT)
+  const [zoneOpacity, setZoneOpacity] = useState(ZONE_COLOR_OPACITY * 100)
 
-  const colorPalette = useCallback(
-    (count: number) => {
-      const colors = [
-        '#ff0000',
-        '#ff4000',
-        '#fe5c00',
-        '#fd7300',
-        '#fc8613',
-        '#fb9725',
-        '#faa837',
-        '#fab749',
-        '#fac55c',
-        '#fad370'
-      ]
-      const step = maxCount / colors.length
-      const colorIndex = Math.floor(count / step)
-      if (colorIndex > colors.length) {
-        return colors[0]
+  const updateBounds = useCallback((newBounds: LatLngBounds) => {
+    setBounds((prevBounds) => {
+      if (prevBounds !== null && newBounds.equals(prevBounds)) {
+        return prevBounds
       }
-      return colors.reverse()[colorIndex]
-    },
-    [maxCount]
-  )
+      return newBounds
+    })
+  }, [])
 
+  const loadingProgress = (stage: number, total: number) => {
+    setDataLoadingProgress((stage / total) * 100)
+  }
+
+  // Update bounds on map move
+  const map = useMapEvents({
+    moveend: () => {
+      updateBounds(map.getBounds())
+    },
+    zoomend: () => {
+      updateBounds(map.getBounds())
+    }
+  })
+  // Update bounds on first render
+  useEffect(() => {
+    updateBounds(map.getBounds())
+  }, [map, updateBounds])
+
+  // Reset zones when cohortId changes
+  useEffect(() => {
+    setVisibleZones([])
+    setZones([])
+    setLoadedBounds([])
+    setMaxCount(MAX_COUNT_DEFAULT)
+  }, [cohortId])
+
+  // Fetch zones when bounds or nearFilter change
   useEffect(() => {
     const abortController = new AbortController()
-    setDataLoading(true)
-    ;(async () => {
-      const locations = await getAllResults(fetchLocation, {
-        _list: [cohortId],
-        size: LOCATION_FETCH_BATCH_SIZE,
-        signal: abortController.signal
-      })
-      if (locations) {
-        const newZones = locations.map((ft) => ({
-          shape: parseShape(ft.extension?.find((ext) => ext.url === LOCATION_SHAPE_EXTENSION_URL)?.valueString) || [],
-          meta: {
-            count: ft.extension?.find((ext) => ext.url === LOCATION_COUNT_EXTENSION_URL)?.valueInteger || 0,
-            name: ft.name || ''
-          }
-        }))
-        const maxCount =
-          d3.quantile(
-            newZones.map((zone) => zone.meta.count),
-            MAX_COUNT_QUANTILE
-          ) || Math.max(...newZones.map((zone) => zone.meta.count))
-        setZones(newZones)
-        setMaxCount(maxCount)
+    if (bounds) {
+      // If the current viewbox is already covered by the loaded bounds, do not fetch new locations
+      if (isBoundCovered(map, bounds, loadedBounds)) {
         setDataLoading(false)
+        return
       }
-    })()
+
+      ;(async () => {
+        try {
+          setDataLoading(true)
+          setDataLoadingProgress(0)
+          // Fetch fhir locations for the current viewbox using the near filter
+          const nearFilter = computeNearFilter(map, bounds)
+          const locations = await getAllResults(
+            fetchLocation,
+            {
+              _list: [cohortId],
+              size: LOCATION_FETCH_BATCH_SIZE,
+              near: nearFilter,
+              signal: abortController.signal
+            },
+            loadingProgress
+          )
+
+          if (locations) {
+            // updates loaded zones
+            setZones((existingZones) => {
+              // get existing zone names (to avoid duplicates and prevent parsing the shape again)
+              const existingNames = existingZones.map((zone) => zone.meta.name)
+              const newZones = existingZones.concat(
+                locations
+                  .filter((ft) => ft.name && existingNames.indexOf(ft.name) === -1)
+                  .map((ft) => {
+                    return {
+                      shape:
+                        parseShape(
+                          ft.extension?.find((ext) => ext.url === LOCATION_SHAPE_EXTENSION_URL)?.valueString
+                        ) || [],
+                      meta: {
+                        count: ft.extension?.find((ext) => ext.url === LOCATION_COUNT_EXTENSION_URL)?.valueInteger || 0,
+                        name: ft.name || ''
+                      }
+                    }
+                  })
+              )
+              // update the loaded bounds
+              setLoadedBounds((prevLoadedBounds) => {
+                return prevLoadedBounds.concat(bounds)
+              })
+
+              return newZones
+            })
+          }
+          setDataLoading(false)
+        } catch (error) {
+          // TODO use snackbar setMessage to display error instead
+          // except if the error if of type AbortError (which is normal when the user moves the map too fast)
+          console.error(error)
+        }
+      })()
+    }
     return () => {
       cancelPendingRequest(abortController)
     }
-  }, [cohortId])
+  }, [cohortId, bounds, setDataLoading, map, loadedBounds])
+
+  // Update visible zones (and max visible count) when zones or bounds change
+  useEffect(() => {
+    const newVisibleZones = zones.filter((zone) => zone.shape.find((shape) => bounds?.contains(shape)))
+    setVisibleZones(newVisibleZones)
+    const maxCount =
+      d3.quantile(
+        newVisibleZones.map((zone) => zone.meta.count),
+        MAX_COUNT_QUANTILE
+      ) || Math.max(...newVisibleZones.map((zone) => zone.meta.count))
+    setMaxCount(maxCount)
+  }, [zones, bounds])
+
+  /***************************************************** */
+  /*                    RENDERING                        */
+  /***************************************************** */
+
+  const renderLoader = () => {
+    return (
+      dataLoading && (
+        <div
+          style={{
+            position: 'absolute',
+            height: '500px',
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            zIndex: 1000,
+            backgroundColor: '#ffffffb2'
+          }}
+        >
+          {dataLoadingProgress ? <CircularProgressWithLabel value={dataLoadingProgress} /> : <CircularProgress />}
+        </div>
+      )
+    )
+  }
+
+  const renderControls = () => {
+    return (
+      <div
+        className="leaflet-control leaflet-bar"
+        style={{
+          position: 'absolute',
+          bottom: '10px',
+          left: '120px',
+          backgroundColor: 'white',
+          padding: '10px',
+          borderRadius: '5px',
+          width: '100px',
+          height: '120px',
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+          zIndex: 1000
+        }}
+      >
+        <Typography variant="h6" color={'black'}>
+          Controles
+        </Typography>
+        <div>
+          <div>Visibilité :</div>
+          <div style={{ display: 'flex', justifyContent: 'center', width: '100%', height: '10px' }}>
+            <Slider value={zoneOpacity} onChange={(_, val) => setZoneOpacity(val as number)} />
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   return (
-    <div style={{ width: '100%', padding: '10px' }}>
-      {dataLoading ? (
-        <div
-          style={{ height: '500px', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-        >
-          <CircularProgress />
+    <div>
+      {renderLoader()}
+      {SHOW_OPACITY_CONTROL && renderControls()}
+      {DEBUG_SHOW_LOADED_BOUNDS && (
+        <div>
+          {loadedBounds &&
+            loadedBounds.map((b, i) => (
+              <Rectangle
+                key={`mesh_${i}`}
+                pathOptions={{
+                  color: '#0000ff',
+                  opacity: 1,
+                  fillOpacity: 0.5
+                }}
+                bounds={b}
+              />
+            ))}
+          {bounds &&
+            explodeBoundsIntoMesh(map, bounds, 1000).map((b, i) => (
+              <Rectangle
+                key={`mesh_${i}`}
+                pathOptions={{
+                  color: '#ff00ff',
+                  opacity: 1,
+                  fillOpacity: 0.5
+                }}
+                bounds={b}
+              />
+            ))}
         </div>
-      ) : (
-        <MapContainer
-          preferCanvas={true}
-          renderer={L.canvas()}
-          center={center}
-          zoom={10}
-          scrollWheelZoom={true}
-          style={{ height: '500px', width: '100%' }}
-        >
-          <TileLayer attribution={MAP_COPYRIGHTS} url={TILE_URL} />
-          {zones.map((zone, i) => (
-            <Polygon
-              key={i}
-              pathOptions={{ color: colorPalette(zone.meta.count), fillOpacity: ZONE_COLOR_OPACITY }}
-              positions={zone.shape}
-            >
-              <Tooltip sticky>
-                <div>
-                  <b>{zone.meta.name}</b>
-                </div>
-                <div>Total: {zone.meta.count}</div>
-              </Tooltip>
-            </Polygon>
-          ))}
-        </MapContainer>
       )}
+      {visibleZones.map((zone, i) => (
+        <Polygon
+          key={i}
+          pathOptions={{
+            color: colorize(COLOR_PALETTE, zone.meta.count, maxCount),
+            opacity: (BORDER_RELATIVE_OPACITY * zoneOpacity) / 100,
+            fillOpacity: zoneOpacity / 100
+          }}
+          positions={zone.shape}
+        >
+          <Tooltip sticky>
+            <div>
+              <b>{zone.meta.name}</b>
+            </div>
+            <div>Total: {zone.meta.count}</div>
+          </Tooltip>
+        </Polygon>
+      ))}
+      <MapLegend maxCount={maxCount} />
+    </div>
+  )
+}
+
+const MapLegend = (props: { maxCount: number }) => {
+  const { maxCount } = props
+  return (
+    <div
+      className="leaflet-control leaflet-bar"
+      style={{
+        position: 'absolute',
+        bottom: '10px',
+        left: '10px',
+        backgroundColor: 'white',
+        padding: '10px',
+        borderRadius: '5px',
+        width: '100px',
+        boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+        zIndex: 1000
+      }}
+    >
+      <div style={{ color: 'black', fontWeight: 'bold', marginBottom: '4px' }}>Legende</div>
+      <div>
+        <div>Densité</div>
+        <div style={{ display: 'flex', justifyContent: 'center', width: '100%', height: '10px' }}>
+          {COLOR_PALETTE.map((color, i) => (
+            <div key={i} style={{ flexGrow: 1, backgroundColor: color }} />
+          ))}
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between'
+          }}
+        >
+          <div>1</div>
+          <div>{Math.max(Math.round(maxCount / 10), 1) * 10}+</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const LocationMap = (props: LocationMapProps) => {
+  const { cohortId, center = DEFAULT_MAP_CENTER } = props
+
+  return (
+    <div style={{ width: '100%', padding: '10px', position: 'relative' }}>
+      <MapContainer
+        preferCanvas={true}
+        renderer={L.canvas()}
+        center={center}
+        zoom={10}
+        scrollWheelZoom={true}
+        style={{ height: '500px', width: '100%' }}
+      >
+        <IrisZones cohortId={cohortId} />
+        <TileLayer attribution={MAP_COPYRIGHTS} url={TILE_URL} />
+      </MapContainer>
     </div>
   )
 }
--- src/components/Dashboard/Preview/LocationMap/utils.ts
+++ src/components/Dashboard/Preview/LocationMap/utils.ts
@@ -1,5 +1,8 @@
-import { LatLngTuple } from 'leaflet'
+import L, { LatLngBounds, LatLngTuple } from 'leaflet'
 
+/**
+ * Transform the string polygon description retrieved by FHIR (POLYGON((lat lng, lat lng, ...))) into an array of LatLngTuple
+ */
 export const parseShape = (polygons?: string): LatLngTuple[] | null => {
   if (!polygons) return null
   const shapes: LatLngTuple[][] = polygons
@@ -21,4 +24,65 @@ export const parseShape = (polygons?: string): LatLngTuple[] | null => {
   } else {
     return shapes[0]
   }
+}
+
+/**
+ * Compute the near Location param (https://www.hl7.org/fhir/R4/location.html#search) from the map viewbox bounds
+ */
+export const computeNearFilter = (map: L.Map, bounds: LatLngBounds): string => {
+  const center = bounds.getCenter()
+  const r1 = map.distance(center, bounds.getNorthEast())
+  const r2 = map.distance(center, bounds.getSouthEast())
+  return `${center.lat}|${center.lng}|${Math.round(Math.max(r1, r2) / 1000)}`
+}
+
+/**
+ * Explode the map viewbox bounds into a mesh of smaller bounds
+ */
+export const explodeBoundsIntoMesh = (map: L.Map, bounds: LatLngBounds, meshUnitSize: number): LatLngBounds[] => {
+  const ne = map.project(bounds.getNorthEast())
+  const sw = map.project(bounds.getSouthWest())
+  const width = ne.x - sw.x
+  const height = sw.y - ne.y
+  const widthDistance = map.distance(bounds.getNorthEast(), bounds.getNorthWest())
+  const pixelToDistanceRatio = widthDistance / width
+  const heightDistance = height * pixelToDistanceRatio
+  const xSteps = Math.min(Math.floor(widthDistance / meshUnitSize), 100)
+  const ySteps = Math.min(Math.floor(heightDistance / meshUnitSize), 100)
+  const xStepSize = width / xSteps
+  const yStepSize = height / ySteps
+  const mesh: LatLngBounds[] = []
+  for (let i = 1; i < xSteps - 1; i++) {
+    for (let j = 1; j < ySteps - 1; j++) {
+      const bsw = map.unproject([sw.x + i * xStepSize, sw.y - j * yStepSize])
+      const bne = map.unproject([sw.x + (i + 1) * xStepSize, sw.y - (j + 1) * yStepSize])
+      mesh.push(new LatLngBounds(bsw, bne))
+    }
+  }
+  return mesh
+}
+
+export const isBoundCovered = (map: L.Map, bounds: LatLngBounds, loadedBounds: LatLngBounds[]): boolean => {
+  // Explode the current viewbox bounds into a mesh of smaller bounds
+  const boundMesh = explodeBoundsIntoMesh(map, bounds, 1000)
+  // Search for a mesh unit that is not covered by the already loaded bounds
+  const meshUnitNotCovered = boundMesh.find((b) => {
+    if (loadedBounds.some((lb) => lb.contains(b))) {
+      return false
+    }
+    return true
+  })
+  return !meshUnitNotCovered
+}
+
+/**
+ * Colorize a count based on a color palette
+ */
+export const colorize = (colorPalette: string[], count: number, maxCount: number): string => {
+  const step = maxCount / colorPalette.length
+  const colorIndex = Math.floor(count / step)
+  if (colorIndex > colorPalette.length) {
+    return colorPalette[colorPalette.length - 1]
+  }
+  return colorPalette[colorIndex]
 }
