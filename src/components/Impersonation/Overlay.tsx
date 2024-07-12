53195ee0746f35e5c7eba16f5cdfe2d6ef03a730
Paul Bui-Quang <paul.bui-quang@aphp.fr>
2024-07-12 10:53:17 +0200

    fix(impersonifcation): retrieve current impersonification info in local storage
    fix(impersonifcation): retrieve current impersonification info in localstorage + add overlay position swap

--- src/components/Impersonation/Overlay.tsx
+++ src/components/Impersonation/Overlay.tsx
@@ -1,11 +1,13 @@
-import { Button } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, IconButton } from '@mui/material'
 import { useAppSelector, useAppDispatch } from 'state'
 import { impersonate } from 'state/me'
+import SwapVertIcon from '@mui/icons-material/SwapVert'
 
 export default () => {
   const me = useAppSelector((state) => state.me)
   const dispatch = useAppDispatch()
+  const [bottomPosition, setBottomPosition] = useState(true)
 
   const stopImpersonation = () => {
     dispatch(impersonate(undefined))
@@ -19,7 +21,8 @@ export default () => {
             width: '100%',
             backgroundColor: '#ffac41',
             position: 'fixed',
-            bottom: 0,
+            bottom: bottomPosition ? 0 : undefined,
+            top: bottomPosition ? undefined : 0,
             left: 0,
             height: '3em',
             display: 'flex',
@@ -35,6 +38,11 @@ export default () => {
             }}
           >
             <Button onClick={stopImpersonation}>Arrêter</Button>
+          </div>
+          <div style={{ position: 'absolute', right: '1em' }}>
+            <IconButton onClick={() => setBottomPosition((prev) => !prev)}>
+              <SwapVertIcon color="primary" />
+            </IconButton>
           </div>
         </div>
       </>
