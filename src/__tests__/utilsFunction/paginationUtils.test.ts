896778afaeac756e3a2d86017404fa24467bcfd6
Salah-BOUYAHIA <salah.bouyahia-ext@aphp.fr>
2024-09-26 14:57:59 +0200

    feat: add test for pagination

---
+++ src/__tests__/utilsFunction/paginationUtils.test.ts
@@ -0,0 +1,44 @@
+import { checkIfPageAvailable, handlePageError } from 'utils/paginationUtils'
+import { vi } from 'vitest'
+
+describe('checkIfPageAvailable', () => {
+  it('should not return a dispatch with error ', () => {
+    const count = 0
+    const currentPage = 1
+    const setPage = vi.fn()
+    const dispatch = vi.fn()
+    const rowsPerPage = 20
+
+    checkIfPageAvailable(count, currentPage, setPage, dispatch, rowsPerPage)
+    expect(dispatch).not.toHaveBeenCalled()
+  })
+  it('should return dispatch with error', () => {
+    const count = 300
+    const currentPage = 155
+    const setPage = vi.fn()
+    const dispatch = vi.fn()
+    const rowsPerPage = 20
+
+    checkIfPageAvailable(count, currentPage, setPage, dispatch, rowsPerPage)
+    expect(dispatch).toHaveBeenCalled()
+  })
+})
+
+describe('handlePageError', () => {
+  it('should not return a dispatch with error ', () => {
+    const page = 1
+    const setPage = vi.fn()
+    const dispatch = vi.fn()
+
+    handlePageError(page, setPage, dispatch)
+    expect(dispatch).not.toHaveBeenCalled()
+  })
+  it('should return dispatch with error', () => {
+    const page = 1001
+    const setPage = vi.fn()
+    const dispatch = vi.fn()
+
+    handlePageError(page, setPage, dispatch)
+    expect(dispatch).toHaveBeenCalled()
+  })
+})
