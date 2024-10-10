68551c5a6e10c9aca16b0fd119ec74ce6e3395cd
Manelle G. <39384110+ManelleG@users.noreply.github.com>
2024-10-10 10:50:13 +0200

    feat: changed encounter info in welcome page - Ref gestion-de-projet#2760 (#1067)

--- src/data/infoMessage.ts
+++ src/data/infoMessage.ts
@@ -18,6 +18,6 @@ export const infoMessages: { id: number; resourceType?: ResourceType; level: lev
     resourceType: ResourceType.ENCOUNTER,
     level: level.INFO,
     message:
-      "Le critère de prise en charge se base sur tous les séjours et passages. Les consultations étant des prises en charge non clôturées, elles n'ont pas de date de fin. Indiquer une durée ou une date de fin de prise en charge exclue ainsi les consultations."
+      "Le critère de prise en charge se base sur tous les séjours et passages. Les consultations étant des prises en charge non clôturées, elles n'ont pas de date de fin. Indiquer une durée ou une date de prise en charge exclue ainsi les consultations."
   }
 ]
