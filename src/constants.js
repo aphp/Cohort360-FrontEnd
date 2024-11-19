cef9bd369311a1bb21342f151e7eb57db5d6b109
Paul Bui-Quang <paul.bui-quang@aphp.fr>
2024-11-19 17:38:22 +0100

    fix: add config url env variable

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 const ENV_CONFIG_URL = import.meta.env.DEV ? import.meta.env.VITE_CONFIG_URL : '{VITE_CONFIG_URL}'
-export const CONFIG_URL =
-  ENV_CONFIG_URL === '{' + 'VITE_CONFIG_URL' + '}' ? '/config.json' : ENV_CONFIG_URL || '/config.json'
+export const CONFIG_URL = !!`${ENV_CONFIG_URL}`.match('VITE_CONFIG_URL')
+  ? '/config.json'
+  : `${ENV_CONFIG_URL}` || '/config.json'
 
 export const ACCESS_TOKEN = 'access_token'
 export const REFRESH_TOKEN = 'refresh_token'
