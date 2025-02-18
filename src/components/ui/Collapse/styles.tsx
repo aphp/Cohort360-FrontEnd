ca15adc502296ead6290fb06d0ba51bf9db9e67e
Paul Bui-Quang <paul.bui-quang@aphp.fr>
2025-02-18 18:16:15 +0100

    fix(criterias): allow removing value in number input + fix max width of collapsable options container

--- src/components/ui/Collapse/styles.tsx
+++ src/components/ui/Collapse/styles.tsx
@@ -1,7 +1,8 @@
 import { Collapse, styled } from '@mui/material'
 
 export const CollapseWrapper = styled(Collapse)(() => ({
+  maxWidth: '100%',
   '& .MuiCollapse-wrapperInner > *': {
-    margin: '0 0 1em'
+    margin: '0 0 1em',
   }
 }))
