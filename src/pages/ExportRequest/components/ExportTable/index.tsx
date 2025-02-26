777243533538ae330708133553fc2ba75e49af9e
Salah-BOUYAHIA <salah.bouyahia-ext@aphp.fr>
2025-02-26 17:58:38 +0100

    fix: all selected into columns filter

--- src/pages/ExportRequest/components/ExportTable/index.tsx
+++ src/pages/ExportRequest/components/ExportTable/index.tsx
@@ -241,7 +241,7 @@ const ExportTable: React.FC<ExportTableProps> = ({
               sx={{ width: '500px' }}
               disabled={tableSetting?.isChecked === false}
               disableCloseOnSelect
-              options={['Tous selectionner', ...exportColumns]}
+              options={['Tous sélectionner', ...exportColumns]}
               onChange={(event, newValue) => {
                 if (newValue.includes('Tous sélectionner')) {
                   if (tableSetting?.columns?.length === exportColumns.length) {
@@ -253,7 +253,7 @@ const ExportTable: React.FC<ExportTableProps> = ({
                   onChangeTableSettings(
                     exportTable.name,
                     'columns',
-                    newValue.filter((e) => e !== 'Tous selectionner')
+                    newValue.filter((e) => e !== 'Tous sélectionner')
                   )
                 }
               }}
@@ -264,11 +264,11 @@ const ExportTable: React.FC<ExportTableProps> = ({
               renderOption={(props, option, { selected }) => {
                 const { key, ...optionProps } = props
                 const isChecked =
-                  option === 'Tous selectionner'
+                  option === 'Tous sélectionner'
                     ? tableSetting?.columns?.length === exportColumns.length
                     : tableSetting?.columns?.includes(option)
                 const isIndeterminate =
-                  option === 'Tous selectionner' &&
+                  option === 'Tous sélectionner' &&
                   (tableSetting?.columns?.length ?? 0) > 0 &&
                   (tableSetting?.columns?.length ?? 0) < exportColumns.length
                 return (
