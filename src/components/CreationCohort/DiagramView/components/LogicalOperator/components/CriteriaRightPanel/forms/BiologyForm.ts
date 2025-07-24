93de5186160fb0e0634fc944c3fb43d44bd36e09
aetchego <alizeetche@gmail.com>
2025-07-24 12:06:48 +0200

    oops

--- src/components/CreationCohort/DiagramView/components/LogicalOperator/components/CriteriaRightPanel/forms/BiologyForm.ts
+++ src/components/CreationCohort/DiagramView/components/LogicalOperator/components/CriteriaRightPanel/forms/BiologyForm.ts
@@ -162,7 +162,7 @@ export const form: () => CriteriaForm<ObservationDataType> = () => ({
           label: 'Âge au début de la prise en charge',
           type: 'durationRange',
           buildInfo: {
-            chipDisplayMethodExtraArgs: [{ type: 'string', value: 'Âge au début de la prise en charge' }]
+            chipDisplayMethodExtraArgs: [{ type: 'string', value: 'Âge :' }]
           }
         },
         {
