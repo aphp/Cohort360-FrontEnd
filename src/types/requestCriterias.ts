ace8e3854a857706a22d59707b048c754c7fd70d
aetchego <34280393+aetchego@users.noreply.github.com>
2025-09-09 17:48:59 +0200

    feat(imaging): add bodysite filter - Ref gestion-de-projet#2426

--- src/types/requestCriterias.ts
+++ src/types/requestCriterias.ts
@@ -147,6 +147,7 @@ export const ObservationParamsKeys = {
 }
 
 export const ImagingParamsKeys = {
+  BODYSITE: 'bodysite',
   IPP: 'patient.identifier',
   MODALITY: 'modality',
   NDA: 'encounter.identifier',
