a641834618b7dc45867e93d50db6e4780e796fbe
aetchego <alizeetche@gmail.com>
2025-09-10 12:03:38 +0200

    retry

--- src/pages/Export/index.tsx
+++ src/pages/Export/index.tsx
@@ -9,7 +9,7 @@ import { Grid, CircularProgress, Tooltip, Box } from '@mui/material'
 import SearchInput from 'components/ui/Searchbar/SearchInput'
 import DataTable from 'components/ui/Table'
 import { useNavigate, useSearchParams } from 'react-router-dom'
-import { downloadExport, fetchExportsList } from 'services/aphp/serviceExportCohort'
+import { downloadExport, fetchExportsList, retryExport } from 'services/aphp/serviceExportCohort'
 import { cleanSearchParams } from 'utils/paginationUtils'
 import { Back_API_Response, LoadingStatus } from 'types'
 import { cancelPendingRequest } from 'utils/abortController'
@@ -93,6 +93,11 @@ const Export = () => {
 
   const onDownload = (id: string) => downloadExport(id, controllerRef.current?.signal)
 
+  const onRetry = async (id: string) => {
+    await retryExport(id, controllerRef.current?.signal)
+    handleSearch({ page: 1 })
+  }
+
   useEffect(() => {
     handleSearch({ page: Math.max(1, parseInt(searchParams.get('page') ?? '1', 10)) })
   }, [])
@@ -101,7 +106,7 @@ const Export = () => {
     if (deidentified) navigate('/home')
   }, [deidentified, navigate])
 
-  const table = useMemo(() => mapExportListToTable(exportList?.results ?? [], { onDownload }), [exportList])
+  const table = useMemo(() => mapExportListToTable(exportList?.results ?? [], { onDownload, onRetry }), [exportList])
 
   return (
     <PageContainer>
