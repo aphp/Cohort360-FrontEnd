bfb793859ecd2b8c6bc5ed591f3942d4407f0a3d
Guillaume FORTAINE <guillaume+github@fortaine.com>
2026-01-13 14:19:29 +0100

    fix(samples): block zero patient samples - Ref gestion-de-projet#3117 (#1194)

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,6 +1,7 @@
 import { expect, afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import * as matchers from '@testing-library/jest-dom/matchers'
+import '@testing-library/jest-dom/vitest'
 
 // ajoute les méthodes react-testing-library à Vitest pour l'autocompletion
 expect.extend(matchers)
