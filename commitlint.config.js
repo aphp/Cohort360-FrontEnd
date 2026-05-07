2b9067b4fec4ef8f98c92a2d36b5b3682555062e
César Richard <cesar@crichard.fr>
2026-05-07 15:01:18 +0200

    fix(workflows): adjust commitlint script formatting for clarity
    - Updated the commitlint workflow to improve the readability of the script by using a multi-line format for the sed command.

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [0, 'always', 300],
-    'footer-max-length': [0, 'always', 300]
+    'footer-max-length': [0, 'always', 300],
+    'body-max-line-length': [0]
   }
 }
