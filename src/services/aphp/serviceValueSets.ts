efc5e15f9a3a972519f4389f9734207d53957789
Thomas Fimbel <thomas.fimbel@fr.ibm.com>
2026-06-08 10:33:28 +0200

    fix(valueset): restoring activeOnly after accidental deletion

--- src/services/aphp/serviceValueSets.ts
+++ src/services/aphp/serviceValueSets.ts
@@ -292,7 +292,7 @@ export const searchInValueSets = async (
   const filter = searchValue === HIERARCHY_ROOT ? '' : `&filter=${encodeURIComponent(searchValue)}`
   try {
     const res = await apiFhir.get<FHIR_API_Response<ValueSet>>(
-      `/ValueSet/$expand?url=${valueSetUrls.join(',')}${filter}&excludeNested=false&_tag=text-search-rank&_tag=${LOW_TOLERANCE_TAG}${options}`,
+      `/ValueSet/$expand?activeOnly=true&url=${valueSetUrls.join(',')}${filter}&excludeNested=false&_tag=text-search-rank&_tag=${LOW_TOLERANCE_TAG}${options}`,
       { signal }
     )
     // When searching across multiple ValueSets, don't pass a specific valueSetUrl
