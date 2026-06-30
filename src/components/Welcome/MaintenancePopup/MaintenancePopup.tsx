9e795e974fb7b6a0e720fc81c75e24a4ef278acc
Thomas Fimbel <thomas.fimbel@fr.ibm.com>
2026-06-30 21:55:24 +0200

    feat(homePage): change message popup MEP

--- src/components/Welcome/MaintenancePopup/MaintenancePopup.tsx
+++ src/components/Welcome/MaintenancePopup/MaintenancePopup.tsx
@@ -13,7 +13,7 @@ const MaintenancePopup = ({ open, onClose }: MaintenancePopupProps) => {
   return (
     <Modal open={open} title="Maintenance" color="warning" readonly cancelText="Fermer" onClose={onClose}>
       <Typography textAlign="center">
-        Bien que l'application paraisse opérationnelle, elle est toujours en maintenance. Merci de revenir plus tard.
+        L'application est en phase de test et n'est pas opérationnelle. Merci de vous reconnecter plus tard.
       </Typography>
     </Modal>
   )
