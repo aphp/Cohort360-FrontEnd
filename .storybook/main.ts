597663649b91860350fb09cb98318b8d129245bc
César Richard <cesar@crichard.fr>
2026-09-10 14:39:11 +0200

    chore(deps): passe storybook en 10 et vitest en 4
    Ferme GHSA-82fw-gwwq-j7x9, y compris la copie de @vitest/mocker embarquée par
    storybook 9. Les paquets addon-essentials, addon-interactions, blocks et test
    n'existent plus depuis la fusion dans le paquet coeur.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,9 +5,7 @@ const config: StorybookConfig = {
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
-    '@storybook/addon-interactions'
+    '@chromatic-com/storybook'
   ],
   framework: {
     name: '@storybook/react-vite',
